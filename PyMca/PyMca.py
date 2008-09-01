286716b4b24a4cccc771908d14129c2002b24fc8
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2008-09-01 09:11:29 +0000

    PyMca 4.2.6
    git-svn-id: https://svn.code.sf.net/p/pymca/code@630 a8702ac3-0063-49a2-b157-4a214673c9c1

--- PyMca/PyMca.py
+++ PyMca/PyMca.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-__revision__ = "$Revision: 2.00 $"
+__revision__ = "$Revision: 2.01 $"
 #/*##########################################################################
 # Copyright (C) 2004-2008 European Synchrotron Radiation Facility
 #
@@ -70,7 +70,7 @@ QTVERSION = qt.qVersion()
 from PyMca_Icons import IconDict
 from PyMca_help import HelpDict
 import os
-__version__ = "4.2.6 20080723-snapshot"
+__version__ = "4.2.6"
 if (QTVERSION < '4.0.0') and ((sys.platform == 'darwin') or (QTVERSION < '3.0.0')):
     class SplashScreen(qt.QWidget):
         def __init__(self,parent=None,name="SplashScreen",
