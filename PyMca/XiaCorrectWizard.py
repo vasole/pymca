cb7fea221e943b88801cca3e28b6a24614ca5b0e
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2008-11-02 20:17:24 +0000

    Just add the license ...
    git-svn-id: https://svn.code.sf.net/p/pymca/code@667 a8702ac3-0063-49a2-b157-4a214673c9c1

--- PyMca/XiaCorrectWizard.py
+++ PyMca/XiaCorrectWizard.py
@@ -1,3 +1,28 @@
+# Copyright (C) 2004-2008 European Synchrotron Radiation Facility
+#
+# This file is part of the PyMCA X-ray Fluorescence Toolkit developed at
+# the ESRF by the Beamline Instrumentation Software Support (BLISS) group.
+#
+# This toolkit is free software; you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option) 
+# any later version.
+#
+# PyMCA is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PyMCA; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA 02111-1307, USA.
+#
+# PyMCA follows the dual licensing model of Trolltech's Qt and Riverbank's PyQt
+# and cannot be used as a free plugin for a non-free program. 
+#
+# Please contact the ESRF industrial unit (industry@esrf.fr) if this license 
+# is a problem for you.
+#############################################################################*/
 import PyQt4.Qt as qt
 import string
 import os.path
@@ -113,18 +138,18 @@ class XiaCorrectionWidget(qt.QWizardPage):
 
 
     def addSum(self, detectors= [], name=None):
-	num= self.sumTable.rowCount()
-	self.sumTable.setRowCount(num + 1)
+        num= self.sumTable.rowCount()
+        self.sumTable.setRowCount(num + 1)
 
-	if len(detectors):
-	    itemText= string.join(detectors, " ")
-	else:
-	    itemText= "All"
-	item = self.sumTable.item(num, 0)
-	if item is None:
+        if len(detectors):
+            itemText= string.join(detectors, " ")
+        else:
+            itemText= "All"
+        item = self.sumTable.item(num, 0)
+        if item is None:
             item = qt.QTableWidgetItem("Detectors",
                                     qt.QTableWidgetItem.Type)
-            	self.sumTable.setItem(num, 0, item)
+            self.sumTable.setItem(num, 0, item)
         item.setText(itemText)
 
     def __add(self):
