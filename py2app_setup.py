ae3b99f6e47b45d3222d858a6c88824c9d05580a
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2009-11-20 17:53:39 +0000

    Add py2app and Platypus scripts to the repository
    git-svn-id: https://svn.code.sf.net/p/pymca/code@876 a8702ac3-0063-49a2-b157-4a214673c9c1

---
+++ py2app_setup.py
@@ -0,0 +1,45 @@
+"""
+Script for building the PyMca bundle.
+For a distributable application Platypus is needed
+
+Usage:
+    python py2app_setup.py py2app
+"""
+from distutils.core import setup
+import py2app
+import os
+os.system("/bin/rm -rf dist")
+os.system("/bin/rm -rf build")
+os.system("/bin/rm -rf *.pyc")
+
+PyMcaInstallationDir = "build"
+PyMcaDir =os.path.join(PyMcaInstallationDir, "PyMca")
+#make sure PyMca is freshly built
+cmd = "python setup.py install --install-lib %s" % PyMcaInstallationDir
+if os.system(cmd):
+    print "Error building PyMca"
+    sys.exit(1)
+
+pymcapath = PyMcaDir
+application=os.path.join(pymcapath, "PyMcaMain.py")
+
+#The options below are equivalent to running from the command line
+#python py2app_setup.py py2app --packages=matplotlib,ctypes,h5py,Object3D
+#probably matplotlib and PyOpenGL are properly detected by py2app
+setup(
+    app=[application],
+    options={'py2app':{'packages':['h5py','OpenGL','ctypes','matplotlib','logging','Object3D']}}
+)
+
+#add the data files
+os.system("cp -Rf ./PyMca/attdata ./dist/PyMcaMain.app/Contents/Resources/lib/python2.5")
+os.system("cp -Rf ./PyMca/HTML ./dist/PyMcaMain.app/Contents/Resources/lib/python2.5")
+os.system("cp -f ./PyMca/Scofield1973.dict ./dist/PyMcaMain.app/Contents/Resources/lib/python2.5")
+os.system("cp -f ./PyMca/*ShellRates*.dat ./dist/PyMcaMain.app/Contents/Resources/lib/python2.5")
+os.system("cp -f ./PyMca/*ShellConstants*.dat ./dist/PyMcaMain.app/Contents/Resources/lib/python2.5")
+os.system("cp -f ./PyMca/BindingEnergies.dat ./dist/PyMcaMain.app/Contents/Resources/lib/python2.5")
+os.system("cp -f ./PyMca/McaTheory.cfg ./dist/PyMcaMain.app/Contents/Resources/lib/python2.5")
+os.system("cp -f ./PyMca/changelog.txt ./dist/PyMcaMain.app/Contents/Resources")
+os.system("cp -f ./PyMca/PyMcaSplashImage.png ./dist/PyMcaMain.app/Contents/Resources")
+
+#I should add here a command line call to Platypus ...
