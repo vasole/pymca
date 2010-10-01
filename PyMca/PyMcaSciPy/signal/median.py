f0cb99d34673bda57f6149e3a833af9c24db8bca
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2010-10-01 06:03:59 +0000

    Implement 1D median filter as a particular 2D case.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1126 a8702ac3-0063-49a2-b157-4a214673c9c1

--- PyMca/PyMcaSciPy/signal/median.py
+++ PyMca/PyMcaSciPy/signal/median.py
@@ -1,8 +1,8 @@
 import mediantools
 from numpy import asarray
 
-def medfilt2d(input_data, kernel_size=3):
-    """Median filter two 2-dimensional arrays.
+def medfilt2d(input_data, kernel_size=None):
+    """Median filter for 2-dimensional arrays.
 
   Description:
 
@@ -37,3 +37,41 @@ def medfilt2d(input_data, kernel_size=3):
 
     return mediantools._medfilt2d(image, kernel_size)
 
+def medfilt1d(input_data, kernel_size=None):
+    """Median filter 1-dimensional arrays.
+
+  Description:
+
+    Apply a median filter to the input array using a local window-size
+    given by kernel_size (must be odd).
+
+  Inputs:
+
+    in -- An 1-dimensional input array.
+    kernel_size -- A scalar or an length-2 list giving the size of the
+                   median filter window in each dimension.  Elements of
+                   kernel_size should be odd.  If kernel_size is a scalar,
+                   then this scalar is used as the size in each dimension.
+
+  Outputs: (out,)
+
+    out -- An array the same size as input containing the median filtered
+           result.
+
+    """
+    image = asarray(input_data)
+    oldShape = image.shape
+    image.shape = -1, 1
+    if kernel_size is None:
+        kernel_size = [3, 1]
+    kernel_size = asarray(kernel_size)
+    if len(kernel_size.shape) == 0:
+        kernel_size = [kernel_size.item(), 1]
+    kernel_size = asarray(kernel_size)
+
+    for size in kernel_size:
+        if (size % 2) != 1:
+            raise ValueError, "Kernel_size should be odd."
+    output = mediantools._medfilt2d(image, kernel_size)
+    output.shape = oldShape
+    return output
