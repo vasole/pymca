d0662967468e0ec2fdfa65364e9f77dcfb11b381
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2010-10-05 10:09:36 +0000

    Median filter stack plugin.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1135 a8702ac3-0063-49a2-b157-4a214673c9c1

---
+++ PyMca/Median2DBrowser.py
@@ -0,0 +1,112 @@
+#/*##########################################################################
+# Copyright (C) 2004-2010 European Synchrotron Radiation Facility
+#
+# This file is part of the PyMCA X-ray Fluorescence Toolkit developed at
+# the ESRF by the Beamline Instrumentation Software Support (BLISS) group.
+#
+# This toolkit is free software; you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option) 
+# any later version.
+#
+# PyMCA is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PyMCA; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA 02111-1307, USA.
+#
+# PyMCA follows the dual licensing model of Trolltech's Qt and Riverbank's PyQt
+# and cannot be used as a free plugin for a non-free program. 
+#
+# Please contact the ESRF industrial unit (industry@esrf.fr) if this license 
+# is a problem for you.
+#############################################################################*/
+__author__ = "V.A. Sole - ESRF BLISS Group"
+import sys
+import numpy
+from PyMca import StackBrowser
+from PyMca.PyMcaSciPy.signal import median 
+
+medfilt2d = median.medfilt2d
+qt = StackBrowser.qt
+DEBUG = 0
+
+class MedianParameters(qt.QWidget):
+    def __init__(self, parent=None):
+        qt.QWidget.__init__(self, parent)
+        self.mainLayout = qt.QHBoxLayout(self)
+        self.mainLayout.setMargin(0)
+        self.mainLayout.setSpacing(2)
+        self.label = qt.QLabel(self)
+        self.label.setText("Median filter width: ")
+        self.widthSpin = qt.QSpinBox(self)
+        self.widthSpin.setMinimum(1)
+        self.widthSpin.setMaximum(99)
+        self.widthSpin.setValue(1)
+        self.widthSpin.setSingleStep(2)
+        self.mainLayout.addWidget(self.label)
+        self.mainLayout.addWidget(self.widthSpin)
+
+class Median2DBrowser(StackBrowser.StackBrowser):
+    def __init__(self, *var, **kw):
+        StackBrowser.StackBrowser.__init__(self, *var, **kw)
+        self.setWindowTitle("Image Browser with Median Filter")
+        self._medianParameters = {'use':True,
+                                  'row_width':5,
+                                  'column_width':5}
+        self._medianParametersWidget = MedianParameters(self)
+        self._medianParametersWidget.widthSpin.setValue(5)        
+        self.layout().addWidget(self._medianParametersWidget)
+        self.connect(self._medianParametersWidget.widthSpin,
+                     qt.SIGNAL('valueChanged(int)'),
+                     self.setKernelWidth)
+
+    def setKernelWidth(self, value):
+        kernelSize = numpy.asarray(value)
+        if len(kernelSize.shape) == 0:
+            kernelSize = [kernelSize.item()] * 2
+        self._medianParameters['row_width'] = kernelSize[0]
+        self._medianParameters['column_width'] = kernelSize[1]
+        self._medianParametersWidget.widthSpin.setValue(int(kernelSize[0]))
+        current = self.slider.value()
+        self.showImage(current, moveslider=False)
+
+    def _buildTitle(self, legend, index):
+        a = self._medianParameters['row_width']
+        b = self._medianParameters['column_width']
+        if max(a, b) > 1:
+            return "Median Filter (%d,%d) of %s %d" % (a, b, legend, index)
+        else:
+            return StackBrowser.StackBrowser._buildTitle(self, legend, index)
+
+    def setImageData(self, data, **kw):
+        if self._medianParameters['use']:
+            if max(self._medianParameters['row_width'],
+                   self._medianParameters['column_width']) > 1:
+                data = medfilt2d(data,[self._medianParameters['row_width'],
+                                   self._medianParameters['column_width']])
+        StackBrowser.StackBrowser.setImageData(self, data, **kw)
+
+if __name__ == "__main__":
+    #create a dummy stack
+    nrows = 100
+    ncols = 200
+    nchannels = 1024
+    a = numpy.ones((nrows, ncols), numpy.float)
+    stackData = numpy.zeros((nrows, ncols, nchannels), numpy.float)
+    for i in xrange(nchannels):
+        if i % 10:
+            stackData[:, :, i] = a * i
+        else:
+            stackData[:, :, i] = 10 * a * i
+
+    app = qt.QApplication([])
+    qt.QObject.connect(app, qt.SIGNAL("lastWindowClosed()"),
+                        app,qt.SLOT("quit()"))
+    w = Median2DBrowser()
+    w.setStackDataObject(stackData, index=0)
+    w.show()
+    app.exec_()
---
+++ PyMca/PyMcaPlugins/MedianFilterStackPlugin.py
@@ -0,0 +1,124 @@
+"""
+
+A Stack plugin is a module that will be automatically added to the PyMca stack windows
+in order to perform user defined operations on the data stack.
+
+These plugins will be compatible with any stack window that provides the functions:
+    #data related
+    getStackDataObject
+    getStackData
+    getStackInfo
+    setStack
+
+    #images related
+    addImage
+    removeImage
+    replaceImage
+
+    #mask related
+    setSelectionMask
+    getSelectionMask
+
+    #displayed curves
+    getActiveCurve
+    getGraphXLimits
+    getGraphYLimits
+
+    #information method
+    stackUpdated
+    selectionMaskUpdated
+"""
+import StackPluginBase
+try:
+    from PyMca import Median2DBrowser
+    import PyMca.PyMca_Icons as PyMca_Icons
+except ImportError:
+    import Median2DBrowser
+    import PyMca_Icons
+
+DEBUG = 0
+
+class MedianFilterStackPlugin(StackPluginBase.StackPluginBase):
+    def __init__(self, stackWindow, **kw):
+        StackPluginBase.DEBUG = DEBUG
+        StackPluginBase.StackPluginBase.__init__(self, stackWindow, **kw)
+        self.methodDict = {'Show':[self._showWidget,
+                                   "Image Browser with Median Filter",
+                                   PyMca_Icons.brushselect]}
+        self.__methodKeys = ['Show']
+        self.widget = None
+
+    def stackUpdated(self):
+        if DEBUG:
+            print "StackBrowserPlugin.stackUpdated() called"
+        if self.widget is None:
+            return
+        if self.widget.isHidden():
+            return
+        stack = self.getStackDataObject()
+        self.widget.setStackDataObject(stack, stack_name="Stack Index")
+        mask = self.getStackSelectionMask()
+        self.widget.setSelectionMask(mask)
+
+    def selectionMaskUpdated(self):
+        if self.widget is None:
+            return
+        if self.widget.isHidden():
+            return
+        mask = self.getStackSelectionMask()
+        self.widget.setSelectionMask(mask)
+
+    def mySlot(self, ddict):
+        if DEBUG:
+            print "mySlot ", ddict['event'], ddict.keys()
+        if ddict['event'] == "selectionMaskChanged":
+            self.setStackSelectionMask(ddict['current'])
+        elif ddict['event'] == "addImageClicked":
+            self.addImage(ddict['image'], ddict['title'])
+        elif ddict['event'] == "removeImageClicked":
+            self.removeImage(ddict['title'])
+        elif ddict['event'] == "replaceImageClicked":
+            self.replaceImage(ddict['image'], ddict['title'])
+        elif ddict['event'] == "resetSelection":
+            self.setStackSelectionMask(None)
+
+    #Methods implemented by the plugin
+    def getMethods(self):
+        return self.__methodKeys
+
+    def getMethodToolTip(self, name):
+        return self.methodDict[name][1]
+
+    def getMethodPixmap(self, name):
+        return self.methodDict[name][2]
+
+    def applyMethod(self, name):
+        return apply(self.methodDict[name][0])
+
+    def _showWidget(self):
+        if self.widget is None:
+            self.widget = Median2DBrowser.Median2DBrowser(parent=None,
+                                                    rgbwidget=None,
+                                                    selection=True,
+                                                    colormap=True,
+                                                    imageicons=True,
+                                                    standalonesave=True)
+            self.widget.setKernelWidth(3)
+            self.widget.setSelectionMode(True)
+            qt = Median2DBrowser.qt
+            qt.QObject.connect(self.widget,
+                   qt.SIGNAL('MaskImageWidgetSignal'),
+                   self.mySlot)
+
+        #Show
+        self.widget.show()
+        self.widget.raise_()        
+
+        #update
+        self.stackUpdated()
+
+
+MENU_TEXT = "Median Filter"
+def getStackPluginInstance(stackWindow, **kw):
+    ob = MedianFilterStackPlugin(stackWindow)
+    return ob
