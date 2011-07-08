3b19ec4427c95803c3324ab9c0713c95af6564a3
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2011-07-08 08:36:34 +0000

    Add non-restrictive license to the file.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1384 a8702ac3-0063-49a2-b157-4a214673c9c1

--- PyMca/TiffIO.py
+++ PyMca/TiffIO.py
@@ -1,4 +1,22 @@
+#/*##########################################################################
+# Copyright (C) 2011 European Synchrotron Radiation Facility
+#
+# This file is part of the PyMca X-ray Fluorescence Toolkit developed at
+# the ESRF by the Beamline Instrumentation Software Support (BLISS) group.
+#
+# This toolkit is free software; you can redistribute it and/or modify it 
+# under the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option) 
+# any later version.
+#
+# PyMca is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+#############################################################################*/
 __author__ = "V.A. Sole - ESRF Data Analysis"
+__revision__ = 1384
 
 import sys
 import os
