b03958e5610c7185bb4d91ea2a2d07869857ea09
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2011-10-10 10:14:21 +0000

    Update snapshot date.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1430 a8702ac3-0063-49a2-b157-4a214673c9c1

--- PyMca/PyMcaMain.py
+++ PyMca/PyMcaMain.py
@@ -78,7 +78,7 @@ QTVERSION = qt.qVersion()
 from PyMca_Icons import IconDict
 from PyMca_help import HelpDict
 import os
-__version__ = "4.4.2-snapshot20110909"
+__version__ = "4.4.2-snapshot20111009"
 if (QTVERSION < '4.0.0') and ((sys.platform == 'darwin') or (QTVERSION < '3.0.0')):
     class SplashScreen(qt.QWidget):
         def __init__(self,parent=None,name="SplashScreen",
