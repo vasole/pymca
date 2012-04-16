0a0002003483fda4ed564d5e94342837faa4a946
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2012-04-16 15:30:31 +0000

    Test covariance calculation of PCATools module.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1655 a8702ac3-0063-49a2-b157-4a214673c9c1

---
+++ tests/PCAToolsTest.py
@@ -0,0 +1,75 @@
+import unittest
+import numpy
+
+class testPCATools(unittest.TestCase):
+    def testPCAToolsImport(self):
+        from PyMca import PCATools
+
+    def testPCAToolsCovariance(self):
+        from PyMca.PCATools import getCovarianceMatrix
+        x = numpy.array([[0.0,  2.0,  3.0],
+                         [3.0,  0.0, -1.0],
+                         [4.0, -4.0,  4.0],
+                         [4.0,  4.0,  4.0]])
+        nSpectra = x.shape[0]
+        
+        # calculate covariance using numpy
+        numpyCov = numpy.cov(x.T)
+        numpyAvg = x.sum(axis=0).reshape(-1, 1) / nSpectra
+        tmpArray = x.T - numpyAvg
+        numpyCov2 = numpy.dot(tmpArray, tmpArray.T) / nSpectra
+        numpyAvg = numpyAvg.reshape(1, -1)
+
+        # calculate covariance using PCATools and 2D stack
+        pymcaCov, pymcaAvg, nData = getCovarianceMatrix(x,
+                                                        force=False,
+                                                        center=True)
+        
+        self.assertTrue(numpy.allclose(numpyCov, pymcaCov))
+        self.assertTrue(numpy.allclose(numpyAvg, pymcaAvg))
+        self.assertTrue(nData == nSpectra)
+
+        # calculate covariance using PCATools and 2D stack
+        # while dynamically loading the data
+        pymcaCov, pymcaAvg, nData = getCovarianceMatrix(x,
+                                                        force=True,
+                                                        center=True)
+        
+        self.assertTrue(numpy.allclose(numpyCov, pymcaCov))
+        self.assertTrue(numpy.allclose(numpyAvg, pymcaAvg))
+        self.assertTrue(nData == nSpectra)
+
+        # calculate covariance using PCATools and 3D stack
+        x.shape = 2, 2, -1
+        pymcaCov, pymcaAvg, nData = getCovarianceMatrix(x,
+                                                        force=False,
+                                                        center=True)
+
+        self.assertTrue(numpy.allclose(numpyCov, pymcaCov))
+        self.assertTrue(numpy.allclose(numpyAvg, pymcaAvg))
+        self.assertTrue(nData == nSpectra)
+
+        # calculate covariance using PCATools and 3D stack
+        # while dynamically loading the data
+        x.shape = 2, 2, -1
+        pymcaCov, pymcaAvg, nData = getCovarianceMatrix(x,
+                                                        force=True,
+                                                        center=True)
+
+        self.assertTrue(numpy.allclose(numpyCov, pymcaCov))
+        self.assertTrue(numpy.allclose(numpyAvg, pymcaAvg))
+        self.assertTrue(nData == nSpectra)
+
+def getSuite(auto=True):
+    testSuite = unittest.TestSuite()
+    if auto:
+        testSuite.addTest(\
+            unittest.TestLoader().loadTestsFromTestCase(testPCATools))
+    else:
+        # use a predefined order
+        testSuite.addTest(testPCATools("testPCAToolsImport"))
+        testSuite.addTest(testPCATools("testPCAToolsCovariance"))
+    return testSuite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(getSuite(auto=False))
