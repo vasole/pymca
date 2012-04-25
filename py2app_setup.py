e055c1f65a0334d68e90dc852368be64b5a5f47c
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2012-04-25 15:59:48 +0000

    Adapt script to new layout.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1686 a8702ac3-0063-49a2-b157-4a214673c9c1

--- py2app_setup.py
+++ py2app_setup.py
@@ -13,13 +13,18 @@ os.system("/bin/rm -rf dist")
 os.system("/bin/rm -rf build")
 os.system("/bin/rm -rf *.pyc")
 
-PyMcaInstallationDir = "build"
-PyMcaDir =os.path.join(PyMcaInstallationDir, "PyMca")
+PyMcaInstallationDir = os.path.abspath("build")
+PyMcaDir = os.path.join(PyMcaInstallationDir, "PyMca")
 #make sure PyMca is freshly built
 cmd = "python setup.py install --install-lib %s --install-scripts /tmp" % PyMcaInstallationDir
 if os.system(cmd):
     print "Error building PyMca"
     sys.exit(1)
+
+# awful workaround because py2app picks PyMca form the source directory 
+os.chdir(PyMcaInstallationDir)
+sys.path.insert(0, PyMcaInstallationDir)
+
 
 pymcapath = PyMcaDir
 application=os.path.join(pymcapath, "PyMcaMain.py")
@@ -27,28 +32,19 @@ application=os.path.join(pymcapath, "PyMcaMain.py")
 #The options below are equivalent to running from the command line
 #python py2app_setup.py py2app --packages=matplotlib,ctypes,h5py,Object3D
 #probably matplotlib and PyOpenGL are properly detected by py2app
-PACKAGES = ['h5py','OpenGL','ctypes','matplotlib','logging','Object3D','PyMcaPlugins']
+PACKAGES = ['h5py','OpenGL','ctypes','matplotlib','logging', 'PyMca']
 try:
     import mdp
     PACKAGES.append('mdp')
 except:
     pass
+
 setup(
     app=[application],
     options={'py2app':{'packages':PACKAGES}}
 )
 
-PY_DIR="python%s"  % sys.version[0:3]
-#add the data files
-os.system("cp -Rf ./PyMca/attdata ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -Rf ./PyMca/HTML ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -f ./PyMca/Scofield1973.dict ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -f ./PyMca/*ShellRates*.dat ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -f ./PyMca/*ShellConstants*.dat ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -f ./PyMca/E*.dat ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -f ./PyMca/BindingEnergies.dat ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -f ./PyMca/McaTheory.cfg ./dist/PyMcaMain.app/Contents/Resources/lib/%s" % PY_DIR)
-os.system("cp -f ./PyMca/changelog.txt ./dist/PyMcaMain.app/Contents/Resources")
-os.system("cp -f ./PyMca/PyMcaSplashImage.png ./dist/PyMcaMain.app/Contents/Resources")
+# move to the proper place
+os.system("mv -f ./dist ../dist")
 
 #I should add here a command line call to Platypus ...
