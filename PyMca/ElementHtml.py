a1b19c50d977a2d058dd23fd6e5dcc2aef38fa6d
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2012-06-19 10:25:19 +0000

    Relax license. LGPL instead of GPL.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1798 a8702ac3-0063-49a2-b157-4a214673c9c1

--- PyMca/ElementHtml.py
+++ PyMca/ElementHtml.py
@@ -4,27 +4,18 @@
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
 #
-# This toolkit is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your option)
 # any later version.
 #
-# PyMca is distributed in the hope that it will be useful, but WITHOUT ANY
+# This file is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
 # details.
 #
-# You should have received a copy of the GNU General Public License along with
-# PyMca; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# PyMca follows the dual licensing model of Riverbank's PyQt and cannot be
-# used as a free plugin for a non-free program.
-#
-# Please contact the ESRF industrial unit (industry@esrf.fr) if this license
-# is a problem for you.
 #############################################################################*/
-__revision__ = "$Revision: 1.9 $"
+__author__ = "V.A. Sole - ESRF Data Analysis"
 from PyMca import Elements
 
 class ElementHtml(object):
@@ -287,6 +278,11 @@ class ElementHtml(object):
         
         
 if __name__ == "__main__":
+    #
+    # WARNING: This test method uses PyQt and therefore its license is GPL
+    # If you use the methods of this module and not this test portion, you
+    # can safely use it as LPGL for HTML generation.
+    #
     import sys
     from PyMca import PyMcaQt as qt
     app  = qt.QApplication(sys.argv)
