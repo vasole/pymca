1bc9f2c4529052dd4e851b77734211d8146d263e
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2013-03-26 13:22:26 +0000

    Add the call to Platypus.
    git-svn-id: https://svn.code.sf.net/p/pymca/code@1896 a8702ac3-0063-49a2-b157-4a214673c9c1

--- py2app_setup.py
+++ py2app_setup.py
@@ -9,9 +9,22 @@ from distutils.core import setup
 import py2app
 import os
 import sys
+
+#force a clean build
 os.system("/bin/rm -rf dist")
 os.system("/bin/rm -rf build")
 os.system("/bin/rm -rf *.pyc")
+
+
+#obtain the current PyMca version from the source file
+ffile = open(os.path.join('PyMca', 'PyMcaMain.py'), 'r').readlines()
+for line in ffile:
+    if line.startswith('__version__'):
+        #remove spaces and split
+        __version__ = "%s" % line.replace(' ','').split("=")[-1][:-1]
+        #remove " or ' present
+        __version__ = __version__[1:-1]
+        break
 
 PyMcaInstallationDir = os.path.abspath("build")
 PyMcaDir = os.path.join(PyMcaInstallationDir, "PyMca")
@@ -24,8 +37,6 @@ if os.system(cmd):
 # awful workaround because py2app picks PyMca form the source directory 
 os.chdir(PyMcaInstallationDir)
 sys.path.insert(0, PyMcaInstallationDir)
-
-
 pymcapath = PyMcaDir
 application=os.path.join(pymcapath, "PyMcaMain.py")
 
@@ -38,13 +49,41 @@ try:
     PACKAGES.append('mdp')
 except:
     pass
-
+PY2APP_OPTIONS = {'packages':PACKAGES}
+BUNDLE_ICON = '/Users/sole/Installation/PythonSnake.icns' 
+if os.path.exists(BUNDLE_ICON):
+    PY2APP_OPTIONS['iconfile'] = BUNDLE_ICON
+else:
+    BUNDLE_ICON = None
 setup(
     app=[application],
-    options={'py2app':{'packages':PACKAGES}}
+    options={'py2app':PY2APP_OPTIONS}
 )
 
 # move to the proper place
 os.system("mv -f ./dist ../dist")
 
-#I should add here a command line call to Platypus ...
+#Command line call to Platypus ...
+platypusFile = '/usr/local/bin/platypus'
+if os.path.exists(platypusFile):
+    import subprocess
+    args = [platypusFile,
+            '-R',
+            '-a',
+            'PyMca%s' % __version__,
+            '-o',
+            'Progress Bar',
+            '-p',
+            '/bin/bash',
+            '-V',
+            '%s' % __version__,
+            '-I',
+            'ESRF.sole.PyMca%s' % __version__,
+            '-y', #force overwrite
+            '-f'
+            '/Users/sole/svnsourceforge/pymca/dist/PyMcaMain.app']
+    if BUNDLE_ICON is not None:
+        args.append('-i')
+        args.append(BUNDLE_ICON)
+    args.append('/Users/sole/svnsourceforge/pymca/PlatypusScript')
+    process = subprocess.call(args)
