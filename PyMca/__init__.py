19a414275f0e4d0c3ee2a88ff9c03d823bea1fad
vasole <vasole@a8702ac3-0063-49a2-b157-4a214673c9c1>
2013-12-16 13:55:13 +0000

    Try to put a relative import in the __init__.py
    git-svn-id: https://svn.code.sf.net/p/pymca/code@2099 a8702ac3-0063-49a2-b157-4a214673c9c1

--- PyMca/__init__.py
+++ PyMca/__init__.py
@@ -3,4 +3,8 @@ if os.path.exists(os.path.join(\
     os.path.dirname(os.path.dirname(__file__)), 'py2app_setup.py')):
     raise ImportError('PyMca cannot be imported from source directory')
 
-from PyMca.PyMcaIO import *
+try:
+    from .PyMcaIO import *
+except:
+    print("WARNING importing IO directly")
+    from PyMcaIO import *
