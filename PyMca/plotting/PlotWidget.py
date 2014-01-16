2a4ba2b5fef1c40b076e2eb24cb52c51a74bf7a0
V. Armando Sole <sole@esrf.fr>
2014-01-16 18:39:28 +0100

    Add very basic Qt plot widget

---
+++ PyMca/plotting/PlotWidget.py
@@ -0,0 +1,116 @@
+#/*##########################################################################
+# Copyright (C) 2004-2014 European Synchrotron Radiation Facility
+#
+# This file is part of the PyMca X-ray Fluorescence Toolkit developed at
+# the ESRF by the Software group.
+#
+# This toolkit is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# PyMca is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PyMca; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# PyMca follows the dual licensing model of Riverbank's PyQt and cannot be
+# used as a free plugin for a non-free program.
+#
+# Please contact the ESRF industrial unit (industry@esrf.fr) if this license
+# is a problem for you.
+#############################################################################*/
+__author__ = "V.A. Sole - ESRF Software Group"
+import sys
+import os
+import traceback
+import Plot
+
+#TODO check for PySide
+from PyQt4 import QtCore, QtGui
+if not hasattr(QtCore, "Signal"):
+    QtCore.Signal = QtCore.pyqtSignal
+
+DEBUG = 0
+if DEBUG:
+    Plot.DEBUG = DEBUG
+
+class PlotWidget(QtGui.QWidget, Plot.Plot):
+
+    sigPlotSignal = QtCore.Signal(object)
+
+    def __init__(self, parent=None, backend=None,
+                         legends=False, callback=None, **kw):
+        QtGui.QWidget.__init__(self, parent)
+        Plot.Plot.__init__(self, parent, backend=backend, **kw)
+        self.mainLayout = QtGui.QHBoxLayout(self)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainLayout.addWidget(self.getWidgetHandle())
+        if legends:
+            print("Legends widget to be implemented")
+        self.setGraphTitle("  ")
+        self.setGraphXLabel("X")
+        self.setGraphYLabel("Y")
+        callback = kw.get("callback", None)
+        self.setCallback(callback)
+
+    def showLegends(self, flag=True):
+        print("Legends widget to be implemented")
+
+    def graphCallback(self, ddict=None):
+        if ddict is not None:
+            Plot.Plot.graphCallback(self, ddict)
+            self.sigPlotSignal.emit(ddict)
+
+if __name__ == "__main__":
+    import time
+    if "matplotlib" in sys.argv:
+        from MatplotlibBackend import MatplotlibBackend as backend
+        print("USING matplotlib")
+        time.sleep(1)
+    else:
+        from PyQtGraphBackend import PyQtGraphBackend as backend
+        print("USING PyQtGraph")
+        time.sleep(1)
+    import numpy
+    x = numpy.arange(100.)
+    y = x * x
+    app = QtGui.QApplication([])
+    plot = PlotWidget(backend=backend, legends=True)
+    plot.show()
+    if 1:
+        plot.addCurve(x, y, "dummy")
+        plot.addCurve(x+100, x*x)
+        plot.addCurve(x, -y, "dummy 2")
+        print("Active curve = ", plot.getActiveCurve())
+        print("X Limits = ",     plot.getGraphXLimits())
+        print("Y Limits = ",     plot.getGraphYLimits())
+        print("All curves = ",   plot.getAllCurves())
+        #print("REMOVING dummy")
+        #plot.removeCurve("dummy")
+        plot.insertXMarker(50., draggable=True)
+        #plot.insertYMarker(50., draggable=True)
+    else:
+        # insert a few curves
+        cSin={}
+        cCos={}
+        nplots=50
+        for i in range(nplots):
+            # calculate 3 NumPy arrays
+            x = numpy.arange(0.0, 10.0, 0.1)
+            y = 10*numpy.sin(x+(i/10.0) * 3.14)
+            z = numpy.cos(x+(i/10.0) * 3.14)
+            #build a key
+            a="%d" % i
+            #plot the data
+            cSin[a] = plot.addCurve(x, y, 'y = sin(x)' + a, replot=False)
+            cCos[a] = plot.addCurve(x, z, 'y = cos(x)' + a, replot=False)
+        cCos[a] = plot.addCurve(x, z, 'y = cos(x)' + a, replot=True)
+        plot.insertXMarker(5., draggable=True)
+        plot.insertYMarker(5., draggable=True)
+    print("All curves = ", plot.getAllCurves(just_legend=True))
+    app.exec_()
