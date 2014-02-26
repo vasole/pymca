0b2a51ecafeb0f3f7bc7f017881921452f0d2287
V. Armando Sole <sole@esrf.fr>
2014-02-26 17:25:03 +0100

    Encoding error solved.

--- PyMca/plotting/Plot.py
+++ PyMca/plotting/Plot.py
@@ -80,33 +80,33 @@ colorlist  = [colordict['black'],
 #PyQtGraph symbols ['o', 's', 't', 'd', '+', 'x']
 #
 #Matplotlib symbols:
-#”.” 	point
-#”,” 	pixel
-#“o” 	circle
-#“v” 	triangle_down
-#“^” 	triangle_up
-#“<” 	triangle_left
-#“>” 	triangle_right
-#“1” 	tri_down
-#“2” 	tri_up
-#“3” 	tri_left
-#“4” 	tri_right
-#“8” 	octagon
-#“s” 	square
-#“p” 	pentagon
-#“*” 	star
-#“h” 	hexagon1
-#“H” 	hexagon2
-#“+” 	plus
-#“x” 	x
-#“D” 	diamond
-#“d” 	thin_diamond
-#“|” 	vline
-#“_” 	hline
-#“None” 	nothing
+#"." 	point
+#"," 	pixel
+#"o" 	circle
+#"v" 	triangle_down
+#"^" 	triangle_up
+#"<" 	triangle_left
+#">" 	triangle_right
+#"1" 	tri_down
+#"2" 	tri_up
+#"3" 	tri_left
+#"4" 	tri_right
+#"8" 	octagon
+#"s" 	square
+#"p" 	pentagon
+#"*" 	star
+#"h" 	hexagon1
+#"H" 	hexagon2
+#"+" 	plus
+#"x" 	x
+#"D" 	diamond
+#"d" 	thin_diamond
+#"|" 	vline
+#"_" 	hline
+#"None" 	nothing
 #None 	nothing
-#” “ 	nothing
-#“” 	nothing
+#" " 	nothing
+#"" 	nothing
 #
 
 class Plot(PlotBase.PlotBase):
