23305d7029e791ae333bd23d27e59f099bde1a52
V. Armando Sole <sole@esrf.fr>
2014-02-28 15:33:33 +0100

    Restore flip functionality.

--- PyMca/ExternalImagesWindow.py
+++ PyMca/ExternalImagesWindow.py
@@ -116,10 +116,10 @@ class ExternalImagesWindow(MaskImageWidget.MaskImageWidget):
                                      self._flipLeftRight)
             self._flipMenu.addAction(QString("Flip Image Up-Down"),
                                      self._flipUpDown)
-        #else:
-        #    self.connect(self.graphWidget.hFlipToolButton,
-        #         qt.SIGNAL("clicked()"),
-        #         self.__hFlipIconSignal)
+        else:
+            self.connect(self.graphWidget.hFlipToolButton,
+                 qt.SIGNAL("clicked()"),
+                 self.__hFlipIconSignal)
 
 
     def sizeHint(self):
