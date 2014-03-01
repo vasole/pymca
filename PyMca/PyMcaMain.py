bbb266e422424b51020db40c6c11a4eb9d3e240e
V. Armando Sole <sole@esrf.fr>
2014-03-01 16:56:24 +0100

    Update version number.

--- PyMca/PyMcaMain.py
+++ PyMca/PyMcaMain.py
@@ -103,7 +103,7 @@ from PyMca.PyMca_Icons import IconDict
 from PyMca.PyMca_help import HelpDict
 from PyMca import PyMcaDataDir
 import os
-__version__ = "4.7.1"
+__version__ = "5.0.0-alpha1"
 
 if __name__ == "__main__":
     app = qt.QApplication(sys.argv)
