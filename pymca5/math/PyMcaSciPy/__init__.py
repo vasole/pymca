ea351d026b4ccbb6bc57d437f91184386b6784a4
V. Armando Sole <sole@esrf.fr>
2014-04-09 15:53:37 +0200

    Restore plugins.

--- pymca5/math/PyMcaSciPy/__init__.py
+++
@@ -1,7 +0,0 @@
-__doc__ = """
-PyMca.PyMcaPlugins contains plugins associated to PyMca.
-
-They inherit either from PyMca.Plugin1DBase (for instances of classes
-respecting the PyMca.Plot1DBase interface) or PyMca.StackPluginBase (for
-instances repecting the PyMca.StackBase interface)
-"""
