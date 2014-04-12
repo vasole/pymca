ede2b48093fec272fb5cfed8e32106b5d6efec32
V. Armando Sole <sole@esrf.fr>
2014-04-12 14:38:48 +0200

    Proper import.

--- PyMca5/EPDL97/__init__.py
+++ PyMca5/EPDL97/__init__.py
@@ -18,7 +18,7 @@ GenerateEPDL97TotalCrossSections.py
 Those scripts can be found in your EPDL97 installation directory:
 
 import os
-from PyMca import EPDL97
+from PyMca5 import EPDL97
 print(os.path.dirname(EPDL97.__file__))
 
 """
@@ -27,6 +27,6 @@ __version__ = '1.0'
 
 # The parsing modules
 # force the import here in order to see the available
-# modules when doing from PyMca import EADL97
+# modules when doing from PyMca5 import EADL97
 # followed by dir(EADL97) in an interactive session.
 from . import EADLParser, EADLSubshells, EPDL97Parser
