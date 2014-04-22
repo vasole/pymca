634f2036fce155a30b2be155be0133cb20332e61
V. Armando Sole <sole@esrf.fr>
2014-04-22 15:56:01 +0200

    MIT license.

--- PyMca5/PyMcaPhysics/xas/XASSelfattenuationCorrection.py
+++ PyMca5/PyMcaPhysics/xas/XASSelfattenuationCorrection.py
@@ -1,23 +1,37 @@
-# -*- coding: utf-8 -*-
 #/*##########################################################################
-# Copyright (C) 2004-2014 European Synchrotron Radiation Facility
+#
+# The PyMca X-Ray Fluorescence Toolkit
+#
+# Copyright (c) 2004-2014 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
 #
-# This file is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by the
-# Free Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# This file is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 #############################################################################*/
 __author__ = "Ana Sancho Tomas and V.A. Sole"
-__license__ = "LGPL"
+__license__ = "MIT"
+__doc__ = """This module corrects fuorescence XAS spectra for selfattenuation.
+The implemented algorithm is valid for infinite samples. For state-of-the-art
+XAS analysis you should take a look at dedicated and well-tested packages like
+IFEFFIT or Viper/XANES dactyloscope """
 import copy
 import numpy
 from PyMca5.PyMcaIO import ConfigDict
