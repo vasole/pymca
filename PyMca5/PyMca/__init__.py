67c99f12dbf8f4f6441ad4103b81fd0106f3b32e
V. Armando Sole <sole@esrf.fr>
2014-07-01 16:57:41 +0200

    Add a mechanism to simplify porting existing code.

---
+++ PyMca5/PyMca/__init__.py
@@ -0,0 +1,22 @@
+import os
+import glob
+
+def getPackages(directory):
+    packages = []
+    fileList = glob.glob(os.path.join(directory, "*"))
+    for fileName in fileList:
+        if os.path.isdir(fileName):
+            if os.path.exists(os.path.join(fileName, "__init__.py")):
+                # is a package
+                packages.append(fileName)
+                # that may contain packages
+                packages += getPackages(fileName)                
+    return packages
+
+
+# this is the package level directory PyMca5
+baseDirectory = os.path.dirname(os.path.dirname(__file__))
+__path__ += [baseDirectory]
+for directory in ["PyMcaCore", "PyMcaGraph", "PyMcaGui",
+                  "PyMcaIO", "PyMcaMath", "PyMcaMisc", "PyMcaPhysics"]:
+    __path__ += getPackages(os.path.join(baseDirectory, directory))
