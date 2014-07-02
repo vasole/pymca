2e70f1440209b376b579e4b56725735d74d46383
V. Armando Sole <sole@esrf.fr>
2014-07-02 12:24:27 +0200

    Add comments.

--- PyMca5/__init__.py
+++ PyMca5/__init__.py
@@ -3,8 +3,11 @@ if os.path.exists(os.path.join(\
     os.path.dirname(os.path.dirname(__file__)), 'py2app_setup.py')):
     raise ImportError('PyMca cannot be imported from source directory')
 
+# mandatory modules for compatibility
 from .PyMcaCore import Plugin1DBase, StackPluginBase, PyMcaDirs, DataObject
 
+#convenience modules that could be directly imported
+# using from PyMca5.PyMca import 
 try:
     from .PyMcaIO import specfilewrapper, EdfFile, specfile, ConfigDict
 except:
@@ -13,4 +16,9 @@ except:
 
 from .PyMcaMath.fitting import SpecfitFuns, Gefit, Specfit
 from .PyMcaMath.fitting import SpecfitFunctions
+
+# this one is slow and responsible of the lost of time
 from .PyMcaPhysics import Elements
+
+#all the rest can be imported using from PyMca5.PyMca import ...
+from . import PyMca
