8b4457f5bdb2d4052cef3149d79d2c91bc1eeb19
V. Armando Sole <sole@esrf.fr>
2014-08-08 14:14:50 -0700

    Update changelog.txt

--- PyMca/PyMcaMain.py
+++ PyMca/PyMcaMain.py
@@ -93,7 +93,7 @@ from PyMca.PyMca_Icons import IconDict
 from PyMca.PyMca_help import HelpDict
 from PyMca import PyMcaDataDir
 import os
-__version__ = "4.7.3"
+__version__ = "4.7.4"
 if (QTVERSION < '4.0.0') and (sys.platform == 'darwin'):
     class SplashScreen(qt.QWidget):
         def __init__(self,parent=None,name="SplashScreen",
