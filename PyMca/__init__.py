68e28950eafa2eedd6fef1faed48ce1504b34f3f
V. Armando Sole <sole@esrf.fr>
2014-08-29 16:37:09 -0700

    Cleaner solution to the path modifications.

--- PyMca/__init__.py
+++ PyMca/__init__.py
@@ -3,8 +3,5 @@ if os.path.exists(os.path.join(\
     os.path.dirname(os.path.dirname(__file__)), 'py2app_setup.py')):
     raise ImportError('PyMca cannot be imported from source directory')
 
-try:
-    from .PyMcaIO import *
-except:
-    print("WARNING importing IO directly")
-    from PyMcaIO import *
+__path__ += [os.path.join(os.path.dirname(__file__), "PyMcaIO")]
+
