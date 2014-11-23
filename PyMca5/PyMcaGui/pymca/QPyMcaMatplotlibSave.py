b2a59b0b0f34a210e0a132b31de54db62c53c0c3
V. Armando Sole <sole@esrf.fr>
2014-11-23 23:02:26 +0100

    http://sourceforge.net/p/pymca/bugs/18/

--- PyMca5/PyMcaGui/pymca/QPyMcaMatplotlibSave.py
+++ PyMca5/PyMcaGui/pymca/QPyMcaMatplotlibSave.py
@@ -33,6 +33,37 @@ import os
 import numpy
 import traceback
 
+if sys.platform.startswith("win"):
+    # try to avoid problems with MPLCONFIGDIR being a non-writable directory
+    # http://sourceforge.net/p/pymca/bugs/18/
+    try:
+        if os.getenv("MPLCONFIGDIR") is None:
+            import ctypes
+            from ctypes.wintypes import MAX_PATH
+            # recipe based on: http://bugs.python.org/issue1763#msg62242
+            dll = ctypes.windll.shell32
+            buf = ctypes.create_unicode_buffer(MAX_PATH + 1)
+            if dll.SHGetSpecialFolderPathW(None, buf, 0x0005, False):
+                directory = buf.value
+            else:
+                # the above should have worked
+                home = os.getenv('USERPROFILE')
+                try:
+                    l = len(home)
+                    directory = os.path.join(home, "My Documents")
+                except:
+                    home = '\\'
+                    directory = '\\'
+            if os.path.isdir('%s' % directory):
+                directory = os.path.join(directory, "PyMca")
+            else:
+                directory = os.path.join(home, "PyMca")
+            if not os.path.exists('%s' % directory):
+                os.mkdir('%s' % directory)
+            os.environ[ 'MPLCONFIGDIR' ] = directory
+    except:
+        print("WARNING: Could not set MPLCONFIGDIR.", sys.exc_info()[1])
+
 from matplotlib import cm
 from matplotlib.font_manager import FontProperties
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
