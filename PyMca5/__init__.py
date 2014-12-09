6d480e383176ea027ada068d687faf3dc9217f3c
V. Armando Sole <sole@esrf.fr>
2014-12-09 12:33:55 +0100

    Add a convenience method to get the path of the default preferences file (if any!).

--- PyMca5/__init__.py
+++ PyMca5/__init__.py
@@ -27,45 +27,59 @@ __author__ = "V.A. Sole - ESRF Data Analysis"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-import os
-import sys
 __version__ = "5.0.0~rc7"
 
-def version():
-    return __version__
+import os
+import sys
+if sys.platform.startswith("win"):
+    import ctypes
+    from ctypes.wintypes import MAX_PATH
 
 if os.path.exists(os.path.join(\
     os.path.dirname(os.path.dirname(__file__)), 'py2app_setup.py')):
     raise ImportError('PyMca cannot be imported from source directory')
+
+def version():
+    return __version__
+
+def getDefaultSettingsFile():
+    filename = "PyMca.ini"
+    if sys.platform == 'win32':
+        # recipe based on: http://bugs.python.org/issue1763#msg62242
+        dll = ctypes.windll.shell32
+        buf = ctypes.create_unicode_buffer(MAX_PATH + 1)
+        if dll.SHGetSpecialFolderPathW(None, buf, 0x0005, False):
+            directory = buf.value
+        else:
+            # the above should have worked
+            home = os.getenv('USERPROFILE')
+            try:
+                l = len(home)
+                directory = os.path.join(home, "My Documents")
+            except:
+                home = '\\'
+                directory = '\\'
+        if os.path.isdir('%s' % directory):
+            directory = os.path.join(directory, "PyMca")
+        else:
+            directory = os.path.join(home, "PyMca")
+        if not os.path.exists('%s' % directory):
+            os.mkdir('%s' % directory)
+        finalfile = os.path.join(directory, filename)
+    else:
+        home = os.getenv('HOME')
+        directory = os.path.join(home, "PyMca")
+        if not os.path.exists('%s' % directory):
+            os.mkdir('%s' % directory)
+        finalfile =  os.path.join(directory, filename)
+    return finalfile
 
 # workaround matplotlib MPLCONFIGDIR issues under windows
 if sys.platform.startswith("win"):
     try:
         #try to avoid matplotlib config dir problem under windows
         if os.getenv("MPLCONFIGDIR") is None:
-            import ctypes
-            from ctypes.wintypes import MAX_PATH
-            # recipe based on: http://bugs.python.org/issue1763#msg62242
-            dll = ctypes.windll.shell32
-            buf = ctypes.create_unicode_buffer(MAX_PATH + 1)
-            if dll.SHGetSpecialFolderPathW(None, buf, 0x0005, False):
-                directory = buf.value
-            else:
-                # the above should have worked
-                home = os.getenv('USERPROFILE')
-                try:
-                    l = len(home)
-                    directory = os.path.join(home, "My Documents")
-                except:
-                    home = '\\'
-                    directory = '\\'
-            if os.path.isdir('%s' % directory):
-                directory = os.path.join(directory, "PyMca")
-            else:
-                directory = os.path.join(home, "PyMca")
-            if not os.path.exists('%s' % directory):
-                os.mkdir('%s' % directory)
-            os.environ[ 'MPLCONFIGDIR' ] = directory
+            os.environ['MPLCONFIGDIR'] = os.path.dirname(getDefaultSettingsFile())
     except:
         print("WARNING: Could not set MPLCONFIGDIR.", sys.exc_info()[1])
 
