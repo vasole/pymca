bd18a94ebdbfada480dc0c70f0aee8611d328879
V. Armando Sole <sole@esrf.fr>
2015-02-10 22:32:40 +0100

    Correct issue on dynamically loaded stacks.

--- PyMca5/PyMcaPhysics/xrf/FastXRFLinearFit.py
+++ PyMca5/PyMcaPhysics/xrf/FastXRFLinearFit.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2014 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2015 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -372,8 +372,24 @@ class FastXRFLinearFit(object):
                 nFits += 1
                 A = derivatives[:, [i for i in range(nFree) if i not in badParameters]]
                 #assume we'll not have too many spectra
-                spectra = data[badMask, iXMin:iXMax+1]
-                spectra.shape = badMask.sum(), -1
+                try:
+                    spectra = data[badMask, iXMin:iXMax+1]
+                    spectra.shape = badMask.sum(), -1
+                except TypeError:
+                    # in case of dynamic arrays, two dimensional indices are not
+                    # supported by h5py
+                    spectra = numpy.zeros((int(badMask.sum()), 1 + iXMax - iXMin),
+                                          data.dtype)
+                    selectedIndices = numpy.nonzero(badMask > 0)
+                    tmpData = numpy.zeros((1, 1 + iXMax - iXMin), data.dtype)
+                    oldDataRow = -1
+                    j = 0
+                    for i in range(len(selectedIndices[0])):
+                        j = selectedIndices[0][i]
+                        if j != oldDataRow:
+                            tmpData = data[j]
+                            olddataRow = j
+                        spectra[i] = tmpData[selectedIndices[1][i]]
                 spectra = spectra.T
                 #
                 if config['fit']['stripflag']:
