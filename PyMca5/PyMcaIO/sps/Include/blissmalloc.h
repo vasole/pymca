b8555d0a1f3caaa1313292edce80f2579f4ebb1e
V. Armando Sole <sole@esrf.fr>
2015-03-10 10:33:36 +0100

    Workaround licensecheck issue not recognizing license when more than two spaces follow the line comment character.

--- PyMca5/PyMcaIO/sps/Include/blissmalloc.h
+++ PyMca5/PyMcaIO/sps/Include/blissmalloc.h
@@ -6,24 +6,23 @@
 *   of files designed to interface the shared-data structures used and defined
 *   by the CSS "spec" package with other utility software.
 *
-*   Permission is hereby granted, free of charge, to any person obtaining a
-*   copy of the software in this file and associated header files (the
-*   "Software"), to deal in the Software without restriction, including
-*   without limitation the rights to use, copy, modify, merge, publish,
-*   distribute, sublicense, and/or sell copies of the Software, and to
-*   permit persons to whom the Software is furnished to do so, subject to
-*   the following conditions:
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
 *
-*   The above copyright notice and this permission notice shall be included
-*   in all copies or substantial portions of the Software.
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
 *
-*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-*   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-*   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-*   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-*   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-*   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-*   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *
 ****************************************************************************/
 #if MALLOC_DEBUG
--- PyMca5/PyMcaMath/mva/Lanczos.py
+++ PyMca5/PyMcaMath/mva/Lanczos.py
@@ -1,37 +1,27 @@
-## /************************************************************************
-##
-##   Copyright
-##   Alessandro MIRONE
-##   mirone@esrf.fr
-##
-##   Copyright 2002  by European Synchrotron Radiation Facility, Grenoble,
-##                   France
-##
-##                                ----------
-##
-##                            All Rights Reserved
-##
-##                                ----------
-##
-## Permission to use, copy, modify, and distribute this software and its
-## documentation for any purpose and without fee is hereby granted,
-## provided that the above copyright notice appear in all copies and that
-## both that copyright notice and this permission notice appear in
-## supporting documentation, and that the names of European Synchrotron
-## Radiation Facility or ESRF or SCISOFT not be used in advertising or
-## publicity pertaining to distribution of the software without specific,
-## written prior permission.
-##
-## EUROPEAN SYNCHROTRON RADIATION FACILITY DISCLAIMS ALL WARRANTIES WITH
-## REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF
-## MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL EUROPEAN SYNCHROTRON
-## RADIATION FACILITY OR ESRF BE LIABLE FOR ANY SPECIAL, INDIRECT OR
-## CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
-## DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-## TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-## PERFORMANCE OF THIS SOFTWARE.
-##
-## **************************************************************************/
+#/*##########################################################################
+#
+#
+# Copyright (c) 2002 European Synchrotron Radiation Facility
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#############################################################################*/
 __author__ = "A. Mirone - ESRF SciSoft Group"
 __license__ = "MIT"
 import math
