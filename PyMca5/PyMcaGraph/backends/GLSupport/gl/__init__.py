6dee2db84f6b0900844abf67c8fd229017515f5e
tvincent <thomas.vincent@esrf.fr>
2015-03-20 15:24:04 +0100

    GL: fix for Python3 and fix OSMesa backend

--- PyMca5/PyMcaGraph/backends/GLSupport/gl/__init__.py
+++ PyMca5/PyMcaGraph/backends/GLSupport/gl/__init__.py
@@ -64,7 +64,7 @@ except NameError:
 def testGL():
     """Test if required OpenGL version and extensions are available."""
     version = glGetString(GL_VERSION).split()[0]  # get version number
-    major, minor = map(int, version.split('.')[0:2])
+    major, minor = int(version[0]), int(version[2])
     if major < 2 or (major == 2 and minor < 1):
         raise RuntimeError(
             "Requires at least OpenGL version 2.1, running with %s" % version)
