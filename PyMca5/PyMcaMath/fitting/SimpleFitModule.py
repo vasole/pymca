085d14b76c055705cfcfd283f803cef10ed415c7
V. Armando Sole <sole@esrf.fr>
2015-06-29 17:31:19 +0200

    Correct bug using anchors in customized fit.

--- PyMca5/PyMcaMath/fitting/SimpleFitModule.py
+++ PyMca5/PyMcaMath/fitting/SimpleFitModule.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2014 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2015 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -324,7 +324,7 @@ class SimpleFit(object):
                 ravelled.shape = -1
                 for channel in self._fitConfiguration['fit']['stripanchorslist']:
                     if channel <= ravelled[0]:continue
-                    index = numpy.nonzero(ravelled >= channel)
+                    index = numpy.nonzero(ravelled >= channel)[0]
                     if len(index):
                         index = min(index)
                         if index > 0:
