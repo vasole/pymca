ee6205fa3a359aa3024516a7622628afb4496f3e
V. Armando Sole <sole@esrf.fr>
2016-06-28 14:21:52 +0200

    Add rate law plugin

---
+++ PyMca5/PyMcaPlugins/RateLawPlugin.py
@@ -0,0 +1,135 @@
+#/*##########################################################################
+# Copyright (C) 2004-2016 V.A. Sole, European Synchrotron Radiation Facility
+#
+# This file is part of the PyMca X-ray Fluorescence Toolkit developed at
+# the ESRF by the Software group.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#############################################################################*/
+__author__ = "V.A. Sole - ESRF Data Analysis"
+__contact__ = "sole@esrf.fr"
+__license__ = "MIT"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+import numpy
+try:
+    from PyMca5 import Plugin1DBase
+except ImportError:
+    from . import Plugin1DBase
+
+try:
+    from PyMca5.PyMcaGui.math.fitting import RateLawWindow
+except ImportError:
+    print("RateLawPlugin problem")
+
+class RateLawPlugin(Plugin1DBase.Plugin1DBase):
+    def __init__(self, plotWindow, **kw):
+        Plugin1DBase.Plugin1DBase.__init__(self, plotWindow, **kw)
+        self.methodDict = {}
+
+        text = "Graphical calculation of Rate Laws"
+        function = self.calculate
+        info = text
+        icon = None
+        self.methodDict["Calculate"] =[function,
+                                       info,
+                                       icon]
+        self.widget = None
+
+    #Methods to be implemented by the plugin
+    def getMethods(self, plottype=None):
+        """
+        A list with the NAMES  associated to the callable methods
+        that are applicable to the specified plot.
+
+        Plot type can be "SCAN", "MCA", None, ...
+        """
+        names = list(self.methodDict.keys())
+        names.sort()
+        return names
+
+    def getMethodToolTip(self, name):
+        """
+        Returns the help associated to the particular method name or None.
+        """
+        return self.methodDict[name][1]
+
+    def getMethodPixmap(self, name):
+        """
+        Returns the pixmap associated to the particular method name or None.
+        """
+        return self.methodDict[name][2]
+
+    def applyMethod(self, name):
+        """
+        The plugin is asked to apply the method associated to name.
+        """
+        self.methodDict[name][0]()
+        return
+
+    def calculate(self):
+        #get active curve
+        activeCurve = self.getActiveCurve()
+        if activeCurve is None:
+            raise ValueError("Please select an active curve")
+            return
+        x, y, legend0, info = activeCurve[:4]
+        xmin, xmax = self.getGraphXLimits()
+        idx = (x >= xmin) & (x <= xmax)
+        x = x[idx]
+        y = y[idx]
+        if self.widget is None:
+            parent = None
+            self.widget = RateLawWindow.RateLawWindow(parent)
+        self.widget.setSpectrum(x, y,
+                                legend=legend0,
+                                xlabel=info["xlabel"],
+                                ylabel=info["ylabel"])
+        self.widget.show()
+        self.widget.raise_()
+
+MENU_TEXT = "Rate Law"
+def getPlugin1DInstance(plotWindow, **kw):
+    ob = RateLawPlugin(plotWindow)
+    return ob
+
+if __name__ == "__main__":
+    import sys
+    import os
+    from PyMca5.PyMcaGui import PyMcaQt as qt
+    from PyMca5.PyMcaGui import PlotWindow
+    # first order, k = 4.820e-04
+    x = [0, 600, 1200, 1800, 2400, 3000, 3600]
+    y = [0.0365, 0.0274, 0.0206, 0.0157, 0.0117, 0.00860, 0.00640]
+    order = "First"
+    slope = "0.000482"
+    print("Expected order: First")
+    print("Expected slope: 0.000482")
+    sigmay = None
+    app = qt.QApplication([])
+    plot = PlotWindow.PlotWindow()
+    plot.setPluginDirectoryList([os.path.dirname(__file__)])
+    plot.getPlugins()
+    plot.addCurve(x, y, "Test Data")
+    plot.show()
+    plugin = getPlugin1DInstance(plot)
+    for method in plugin.getMethods():
+        print(method, ":", plugin.getMethodToolTip(method))
+    #plugin.applyMethod(plugin.getMethods()[1])    
+    app.exec_()
