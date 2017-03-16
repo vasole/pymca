8fa45e907b3332fef132b00c37e431b66dd32838
V. Armando Solé <sole@esrf.fr>
2017-03-16 12:24:49 +0100

    Support multiple blocks.

--- PyMca5/PyMcaIO/JcampFileParser.py
+++ PyMca5/PyMcaIO/JcampFileParser.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2015 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2017 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -30,14 +30,61 @@ __author__ = "V.A. Sole - ESRF Data Analysis"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+import os
 import sys
 import numpy
 from PyMca5.PyMcaIO import JcampReader
 from PyMca5.PyMcaIO import SpecFileAbstractClass
+if sys.version < "3":
+    import StringIO.StringIO as StringIO
+else:
+    from io import StringIO
 
 class JcampFileParser(SpecFileAbstractClass.SpecFileAbstractClass):
-    def __init__(self, filename):
-        instance = JcampReader.JcampReader(filename)
+    def __init__(self, filename, single=False):
+        # get the number of entries in the file
+        print("RECEIVED = ", single)
+        self.__lastEntryData = -1
+        self._scanLimits = []
+        f = open(filename, "r")
+        entryStarted = False
+        current = f.tell()
+        line = f.readline()
+        nLines = 0
+        while len(line):
+            if entryStarted:
+                if line.startswith("##END="):
+                    lineEnd = nLines
+                    self._scanLimits.append((start, current, lineStart, lineEnd))
+                    entryStarted = False
+                    if single:
+                        break
+            elif line.startswith("##TITLE"):
+                start = current
+                lineStart = nLines
+                entryStarted = True
+            nLines += 1
+            current = f.tell()
+            line = f.readline()
+        f.close()
+        self._filename = os.path.abspath(filename)
+        print("N entries = ", len(self._scanLimits))
+        print("0 ENTRY = ", self._scanLimits[0])
+        self._parseEntryData(0)
+
+    def _parseEntryData(self, idx):
+        if idx == self.__lastEntryData:
+            # nothing to be done
+            return
+        if (idx < 0) or (idx >= len(self._scanLimits)):
+            raise IndexError("Only %d entries in file. Requested %d" % (len(self._scanLimits), idx))
+        #get the relevant file section
+        f = open(self._filename, "r")
+        start, end = self._scanLimits[idx][0:2]
+        f.seek(start)
+        scanBuffer = StringIO(f.read(1 + end - start))
+        f.close()
+        instance = JcampReader.JcampReader(scanBuffer)
         info = instance.info
         jcampDict = info
         x, y = instance.data
@@ -53,7 +100,7 @@ class JcampFileParser(SpecFileAbstractClass.SpecFileAbstractClass):
         data[:, 0] = x
         data[:, 1] = y
         self.scandata = []
-        scanheader = ["#S 1 %s" % title]
+        scanheader = ["#S %d %s" % (2*idx + 1, title)]
         scanheader.append("#N 2")
         scanheader.append("#L %s  %s" % (xLabel, yLabel))
         scanData = JCAMPFileScan(data,
@@ -62,7 +109,7 @@ class JcampFileParser(SpecFileAbstractClass.SpecFileAbstractClass):
                                  labels=[xLabel, yLabel],
                                  fileheader=fileheader)
         self.scandata.append(scanData)
-        scanheader = ["#S 2 %s" % title]
+        scanheader = ["#S %d %s" % (2*idx + 2, title)]
         if jcampDict['XYDATA'].upper() ==  '(X++(Y..Y))':
             # we can deal with the X axis via its calibration
             scanheader.append("#@CHANN %d  %d  %d  1" % (len(x), 0, len(x) - 1))
@@ -73,15 +120,20 @@ class JcampFileParser(SpecFileAbstractClass.SpecFileAbstractClass):
                                                       #labels=[xLabel, yLabel],
                                                       fileheader=fileheader)
         self.scandata.append(scanData)
+        self.__lastEntryData = idx
 
     def __getitem__(self, item):
-        return self.scandata[item]
+        if item < 0:
+            item = self.scanno() - item
+        idx = item // 2
+        self._parseEntryData(idx)
+        return self.scandata[item % 2]
 
     def list(self):
-        return "1:%d" % len(self.scandata)
+        return '1:%d' % self.scanno()
 
     def scanno(self):
-        return len(self.scandata)
+        return len(self.scandata) * len(self._scanLimits)
 
 class JCAMPFileScan(SpecFileAbstractClass.SpecFileAbstractScan):
     def __init__(self, data, scantype="SCAN",
