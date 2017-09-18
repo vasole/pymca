3583dd77b079baf7daa0ab6afef1b7e6a55f1d05
Pierre <pierre.knobel@esrf.fr>
2017-09-18 14:25:22 +0200

    Modify Plot1dView to use a Plot1D with a PluginsToolButton

--- PyMca5/PyMcaGui/io/hdf5/Hdf5DatasetView.py
+++ PyMca5/PyMcaGui/io/hdf5/Hdf5DatasetView.py
@@ -25,21 +25,74 @@
 #############################################################################*/
 """The :class:`HDF5DatasetView` widget in this module aims to be used
 instead of :class:`HDF5DatasetTable` in :class:`QNexusWidget` for
-visualization of HDF5 datasets."""
+visualization of HDF5 datasets.
+
+It uses the silx :class:`DataViewerFrame` widget with views modified
+to handle plugins."""
 __author__ = "P. Knobel - ESRF Data Analysis"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+
+
+import os
+
+import PyMca5
 from PyMca5.PyMcaGui import PyMcaQt as qt
 from PyMca5.PyMcaGui import CloseEventNotifyingWidget
 
+from PyMca5.PyMcaGui.PluginsToolButton import PluginsToolButton
+
 from silx.gui.data import DataViewerFrame
+from silx.gui.data import DataViews
+from silx.gui.plot import Plot1D
+
+
+PLUGINS_DIR = []
+if os.path.exists(os.path.join(os.path.dirname(PyMca5.__file__), "PyMcaPlugins")):
+    from PyMca5 import PyMcaPlugins
+    PLUGINS_DIR = os.path.dirname(PyMcaPlugins.__file__)
+else:
+    directory = os.path.dirname(__file__)
+    while True:
+        if os.path.exists(os.path.join(directory, "PyMcaPlugins")):
+            PLUGINS_DIR.append(os.path.join(directory, "PyMcaPlugins"))
+            break
+        directory = os.path.dirname(directory)
+        if len(directory) < 5:
+            break
+
+userPluginsDirectory = PyMca5.getDefaultUserPluginsDirectory()
+if userPluginsDirectory is not None:
+    PLUGINS_DIR.append(userPluginsDirectory)
+
+
+class Plot1DWithPlugins(Plot1D):
+    def __init__(self, parent=None):
+        Plot1D.__init__(self, parent)
+
+        self._toolbar = qt.QToolBar(self)
+        self.addToolBar(self._toolbar)
+        pluginsToolButton = PluginsToolButton(plot=self)
+
+        if PLUGINS_DIR:
+            pluginsToolButton.getPlugins(
+                    method="getPlugin1DInstance",
+                    directoryList=PLUGINS_DIR)
+        self._toolbar.addWidget(pluginsToolButton)
+
+
+class Plot1DViewWithPlugins(DataViews._Plot1dView):
+    def createWidget(self, parent):
+        return Plot1DViewWithPlugins(parent=parent)
 
 
 class DataViewerFrameWithPlugins(DataViewerFrame.DataViewerFrame):
     def __init__(self, parent=None):
         DataViewerFrame.DataViewerFrame.__init__(self, parent)
-        # self.removeView()
-        # self.addView()
+
+        # Replace Plot1D with a modified Plot1D with plugin support
+        self.removeView(DataViews._Plot1dView)
+        self.addView(Plot1DViewWithPlugins)
 
 
 class Hdf5DatasetView(CloseEventNotifyingWidget.CloseEventNotifyingWidget):
