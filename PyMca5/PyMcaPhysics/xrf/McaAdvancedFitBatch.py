e1143e866b6a0134099bac15ee7f7d738c1bfae7
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-11-22 15:52:27 +0000

    McaAdvancedFitBatch: add quiet option to __init__

--- PyMca5/PyMcaPhysics/xrf/McaAdvancedFitBatch.py
+++ PyMca5/PyMcaPhysics/xrf/McaAdvancedFitBatch.py
@@ -59,7 +59,7 @@ class McaAdvancedFitBatch(object):
                     concentrations=0, fitfiles=1, fitimages=1,
                     filebeginoffset = 0, fileendoffset=0,
                     mcaoffset=0, chunk = None,
-                    selection=None, lock=None, nosave=None):
+                    selection=None, lock=None, nosave=None, quiet=False):
         #for the time being the concentrations are bound to the .fit files
         #that is not necessary, but it will be correctly implemented in
         #future releases
@@ -104,7 +104,7 @@ class McaAdvancedFitBatch(object):
         self.mcaOffset = mcaoffset
         self.chunk     = chunk
         self.selection = selection
-
+        self.quiet = quiet
 
 
     def setFileList(self,filelist=None):
@@ -245,7 +245,8 @@ class McaAdvancedFitBatch(object):
 
 
     def onNewFile(self,ffile, filelist):
-        self.__log(ffile)
+        if not self.quiet:
+            self.__log(ffile)
 
     def onImage(self,image,imagelist):
         pass
