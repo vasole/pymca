9c125de68515261da12c70adc3cea5c0f91e92e2
Pierre <pierre.knobel@esrf.fr>
2018-03-15 17:14:27 +0100

    fix script (missing fi)

--- ci/travis_osx.sh
+++ ci/travis_osx.sh
@@ -3,16 +3,18 @@
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 
-VENV_DIR=./venv
+    VENV_DIR=./venv
 
 
-brew upgrade python@3;
-PYTHON_EXE=`brew list python@3 | grep "bin/python3$" | head -n 1`;
-# Create virtual env
-$PYTHON_EXE -m venv $VENV_DIR
-source $VENV_DIR/bin/activate
+    brew upgrade python@3;
+    PYTHON_EXE=`brew list python@3 | grep "bin/python3$" | head -n 1`;
+    # Create virtual env
+    $PYTHON_EXE -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
 
-# Alternative python installation using miniconda
-#curl -o miniconda_installer.sh "https://repo.continuum.io/miniconda/Miniconda$PYTHON_VERSION-latest-MacOSX-x86_64.sh"
-#bash miniconda_installer.sh -b -p miniconda
-#export PATH="`pwd`/miniconda/bin":$PATH
+    # Alternative python installation using miniconda
+    #curl -o miniconda_installer.sh "https://repo.continuum.io/miniconda/Miniconda$PYTHON_VERSION-latest-MacOSX-x86_64.sh"
+    #bash miniconda_installer.sh -b -p miniconda
+    #export PATH="`pwd`/miniconda/bin":$PATH
+
+fi
