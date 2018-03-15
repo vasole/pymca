1d1d80831fba2f989c7ad37639546cb226923d40
Pierre <pierre.knobel@esrf.fr>
2018-03-15 17:00:27 +0100

    remove 3.4 and 3.5, and add 3.6 on linux; remove PY2 on OSX

--- ci/travis_osx.sh
+++ ci/travis_osx.sh
@@ -5,17 +5,13 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 
 VENV_DIR=./venv
 
-# Use brew for python3
-if [ "$PYTHON_VERSION" == "3" ];
-then
-    brew upgrade python@3;
-    PYTHON_EXE=`brew list python@3 | grep "bin/python3$" | head -n 1`;
-    # Create virtual env
-    $PYTHON_EXE -m venv $VENV_DIR
-    source $VENV_DIR/bin/activate
-fi
 
-fi
+brew upgrade python@3;
+PYTHON_EXE=`brew list python@3 | grep "bin/python3$" | head -n 1`;
+# Create virtual env
+$PYTHON_EXE -m venv $VENV_DIR
+source $VENV_DIR/bin/activate
+
 # Alternative python installation using miniconda
 #curl -o miniconda_installer.sh "https://repo.continuum.io/miniconda/Miniconda$PYTHON_VERSION-latest-MacOSX-x86_64.sh"
 #bash miniconda_installer.sh -b -p miniconda
