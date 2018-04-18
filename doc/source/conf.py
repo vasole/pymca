f1aa45eb0c7927075e96de60e7f0baad5efc3d07
Pierre <pierre.knobel@esrf.fr>
2018-04-18 16:51:36 +0200

    Typo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,7 +36,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.viewcode'',
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.viewcode',
               'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
