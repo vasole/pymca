a34e24e779e785ed316a3cdb27429c2f16126796
V. Armando Solé <sole@esrf.fr>
2018-04-29 11:50:08 +0200

    Make the implementation clearer.

--- setup.py
+++ setup.py
@@ -848,21 +848,19 @@ classifiers = ["Development Status :: 5 - Production/Stable",
                "Topic :: Scientific/Engineering :: Visualization",
                ]
 
-# install requires with all easy-to-provide GUI functionality
-# plese take a look at requirements.txt for detailed explanation
+# install requires for non-GUI usage
 install_requires = ["numpy",
                     "matplotlib",
                     "fisx>=1.1.4",
-                    "h5py",
-                    "PyOpenGL",
-                    "matplotlib>1.0",
-                    "qtconsole",
-                    "PyQt5",          # either PyQt4 or PySide supported too
-                    "silx>=0.7"]
-if not use_gui():
-    # this is the default
-    # just include requirements for non-GUI usage
-    install_requires = install_requires[:4]
+                    "h5py"]
+if use_gui():
+    # install requires with all easy-to-provide GUI functionality
+    # plese take a look at requirements.txt for detailed explanation
+    install_requires += ["PyOpenGL",
+                        "matplotlib>1.0",
+                        "qtconsole",
+                        "PyQt5",          # either PyQt4 or PySide supported too
+                        "silx>=0.7"]
 
 setup_requires = ["numpy"]
 
