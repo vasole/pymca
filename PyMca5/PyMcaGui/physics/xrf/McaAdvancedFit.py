34ea2a5f5049f6fc294e8ad70285791afd032c1c
V. Armando Solé <sole@esrf.fr>
2018-07-11 18:16:38 +0200

    Do not set icon size.

--- PyMca5/PyMcaGui/physics/xrf/McaAdvancedFit.py
+++ PyMca5/PyMcaGui/physics/xrf/McaAdvancedFit.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2017 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2018 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -2864,7 +2864,6 @@ class McaGraphWindow(PlotWindow):
                                              fit=False, **kw)
         self.setDataMargins(0, 0, 0.025, 0.025)
         self.setPanWithArrowKeys(True)
-        self.setIconSize(qt.QSize(20, 20))
 
         # No context menu by default, execute zoomBack on right click
         plotArea = self.getWidgetHandle()
