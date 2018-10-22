7e26f283e4b066d36c1a6879ab0341051b77f9c0
Pierre <pierre.knobel@esrf.fr>
2018-10-22 14:53:47 +0200

    Refactor backend management: let the user pick
    The gl alternative is only presented if some conditions are met.
    But we cannot be 100% sure that OpenGl will work, so we need to offer mpl alternative.

--- PyMca5/PyMcaPlugins/MaskScatterViewPlugin.py
+++ PyMca5/PyMcaPlugins/MaskScatterViewPlugin.py
@@ -50,27 +50,25 @@ _logger = logging.getLogger(__name__)
 
 
 # Probe OpenGL availability and widget
-backend = "mpl"
+isGlAvailable = False
 try:
     import OpenGL
 except ImportError:
-    backend = "mpl"
     _logger.debug("pyopengl not installed")
 else:
     # sanity check from silx.gui._glutils.OpenGLWidget
     if not hasattr(silx_qt, 'QOpenGLWidget') and\
             (not silx_qt.HAS_OPENGL or
              silx_qt.QApplication.instance() and not silx_qt.QGLFormat.hasOpenGL()):
-        backend = "mpl"
         _logger.debug("qt has a QOpenGLWidget: %s", hasattr(silx_qt, 'QOpenGLWidget'))
         _logger.debug("qt.HAS_OPENGL: %s", silx_qt.HAS_OPENGL)
         _logger.debug("silx_qt.QGLFormat.hasOpenGL(): %s",
                       silx_qt.QApplication.instance() and not silx_qt.QGLFormat.hasOpenGL())
 
     else:
-        backend = "gl"
+        isGlAvailable = True
 
-_logger.debug("Using backend %s", backend)
+_logger.debug("Gl availability: %s", isGlAvailable)
 
 
 class AxesPositionersSelector(qt.QWidget):
@@ -148,45 +146,71 @@ class AxesPositionersSelector(qt.QWidget):
 class MaskScatterViewPlugin(StackPluginBase.StackPluginBase):
     def __init__(self, stackWindow, **kw):
         StackPluginBase.StackPluginBase.__init__(self, stackWindow, **kw)
-        self.methodDict = {'Show': [self._showWidget,
-                                    "Show ROIs",
-                                    None]}
-        self.__methodKeys = ['Show']
-        self._scatterView = None
+        tooltip = "Stack as a scatter plot, using positioners as axes"
+        self.methodDict = {'Show (mpl)': [self._showWidgetMpl,
+                                          tooltip + " (matplotlib backend)",
+                                          None]}
+        self.__methodKeys = ['Show (mpl)']
+        self._backends = ['mpl']
+        if isGlAvailable:
+            self.methodDict['Show (gl)'] = [self._showWidgetGl,
+                                            tooltip + " (OpenGL backend)",
+                                            None]
+            self.__methodKeys.append('Show (gl)')
+            self._backends.append('gl')
 
-        self._xdata = None
-        self._ydata = None
+        self._scatterViews = {"gl": None,
+                              "mpl": None}
+        self._axesSelectors = {"gl": None,
+                               "mpl": None}
 
-    def _buildWidget(self):
-        self._scatterView = ScatterView(parent=None, backend=backend)
-        self._setData()
-        self._scatterView.resetZoom()
-        self._scatterView.getMaskToolsWidget().sigMaskChanged.connect(
+        self._xdata = {"gl": None,
+                       "mpl": None}
+        self._ydata = {"gl": None,
+                       "mpl": None}
+
+    def _buildWidget(self, backend):
+        scatterView = ScatterView(parent=None, backend=backend)
+        self._scatterViews[backend] = scatterView
+
+        self._setData(backend)
+        scatterView.resetZoom()
+        scatterView.getMaskToolsWidget().sigMaskChanged.connect(
                 self._scatterMaskChanged)
 
-        self._axesSelector = AxesPositionersSelector(parent=self._scatterView)
-        self._axesSelectorDock = BoxLayoutDockWidget()
-        self._axesSelectorDock.setWindowTitle('Axes selection')
-        self._axesSelectorDock.setWidget(self._axesSelector)
-        self._scatterView.addDockWidget(qt.Qt.BottomDockWidgetArea, self._axesSelectorDock)
+        self._axesSelectors[backend] = AxesPositionersSelector(parent=scatterView)
+        _axesSelectorDock = BoxLayoutDockWidget()
+        _axesSelectorDock.setWindowTitle('Axes selection')
+        _axesSelectorDock.setWidget(self._axesSelectors[backend])
+        scatterView.addDockWidget(qt.Qt.BottomDockWidgetArea, _axesSelectorDock)
 
-        self._axesSelector.setNumPoints(self._getNumStackPoints())
-        self._axesSelector.fillPositioners(self._getStackPositioners())
-        self._axesSelector.sigSelectionChanged.connect(self._setAxesData)
+        self._axesSelectors[backend].setNumPoints(self._getNumStackPoints())
+        self._axesSelectors[backend].fillPositioners(self._getStackPositioners())
 
-    def _showWidget(self):
-        if self._scatterView is None:
-            self._buildWidget()
+        callback = self._setAxesDataGl if backend == "gl" else self._setAxesDataMpl
+        self._axesSelectors[backend].sigSelectionChanged.connect(callback)
+
+    def _showWidgetMpl(self):
+        self._showWidget(backend="mpl")
+
+    def _showWidgetGl(self):
+        self._showWidget(backend="gl")
+
+    def _showWidget(self, backend):
+        if self._scatterViews[backend] is None:
+            self._buildWidget(backend=backend)
 
         # Show
-        self._scatterView.show()
-        self._scatterView.raise_()
+        self._scatterViews[backend].show()
+        self._scatterViews[backend].raise_()
 
-    def _getStackPositioners(self):
-        info = self.getStackInfo()
-        return info.get("positioners", {})
+    def _setAxesDataMpl(self, xPositioner, yPositioner):
+        self._setAxesData(xPositioner, yPositioner, "mpl")
 
-    def _setAxesData(self, xPositioner, yPositioner):
+    def _setAxesDataGl(self, xPositioner, yPositioner):
+        self._setAxesData(xPositioner, yPositioner, "gl")
+
+    def _setAxesData(self, xPositioner, yPositioner, backend):
         """
 
         :param str xPositioner: motor name, or None
@@ -196,44 +220,45 @@ class MaskScatterViewPlugin(StackPluginBase.StackPluginBase):
         positioners = self._getStackPositioners()
         if xPositioner is not None:
             assert xPositioner in positioners
-            self._xdata = positioners[xPositioner]
+            self._xdata[backend] = positioners[xPositioner]
         else:
-            self._xdata = None
+            self._xdata[backend] = None
         if yPositioner is not None:
             assert yPositioner in positioners
-            self._ydata = positioners[yPositioner]
+            self._ydata[backend] = positioners[yPositioner]
         else:
-            self._ydata = None
-        self._setData()
+            self._ydata[backend] = None
+        self._setData(backend)
 
     @contextmanager
-    def _scatterMaskDisconnected(self):
+    def _scatterMaskDisconnected(self, backend):
         # This context manager allows to call self.setStackSelectionMask
         # without entering an infinite loop, by temporarily disconnecting
         # callbacks from our mask signals.
-        self._scatterView.getMaskToolsWidget().sigMaskChanged.disconnect(
-                self._scatterMaskChanged)
+        callback = self._scatterMaskChangedGl if backend == "gl" else self._scatterMaskChangedMpl
+        self._scatterViews[backend].getMaskToolsWidget().sigMaskChanged.disconnect(
+                callback)
         try:
             yield
         finally:
-            self._scatterView.getMaskToolsWidget().sigMaskChanged.connect(
-                    self._scatterMaskChanged)
+            self._scatterViews[backend].getMaskToolsWidget().sigMaskChanged.connect(
+                    callback)
 
-    def _setData(self):
+    def _setData(self, backend):
         stack_images, stack_names = self.getStackROIImagesAndNames()
         nrows, ncols = stack_images[0].shape
 
         # flatten image
         stackValues = stack_images[0].reshape((-1,))
         # get regular grid coordinates as a 1D array
-        if self._xdata is None or self._ydata is None:
+        if self._xdata[backend] is None or self._ydata[backend] is None:
             defaultX, defaultY = numpy.meshgrid(numpy.arange(ncols),
                                                 numpy.arange(nrows))
             defaultX.shape = stackValues.shape
             defaultY.shape = stackValues.shape
 
-        xdata = self._xdata if self._xdata is not None else defaultX
-        ydata = self._ydata if self._ydata is not None else defaultY
+        xdata = self._xdata[backend] if self._xdata[backend] is not None else defaultX
+        ydata = self._ydata[backend] if self._ydata[backend] is not None else defaultY
 
         if numpy.isscalar(xdata):
             xdata = xdata * numpy.ones_like(stackValues)
@@ -249,48 +274,61 @@ class MaskScatterViewPlugin(StackPluginBase.StackPluginBase):
             _logger.debug("flattening %s array", str(ydata.shape))
             ydata = ydata.reshape((-1,))
 
-        self._scatterView.setData(xdata, ydata, stackValues,
-                                  copy=False)
-        self._scatterView.resetZoom()
+        self._scatterViews[backend].setData(xdata, ydata, stackValues,
+                                            copy=False)
+        self._scatterViews[backend].resetZoom()
 
-    def _isScatterViewVisible(self):
-        if self._scatterView is None:
+    def _isScatterViewVisible(self, backend):
+        if self._scatterViews[backend] is None:
             return False
-        if self._scatterView.isHidden():
+        if self._scatterViews[backend].isHidden():
             return False
         return True
 
-    def _scatterMaskChanged(self):
-        scattermask = self._scatterView.getMaskToolsWidget().getSelectionMask()
+    def _scatterMaskChangedGl(self):
+        self._scatterMaskChanged("gl")
+
+    def _scatterMaskChangedMpl(self):
+        self._scatterMaskChanged("mpl")
+
+    def _scatterMaskChanged(self, backend):
+        scattermask = self._scatterViews[backend].getMaskToolsWidget().getSelectionMask()
         if scattermask is not None:
             shape = self.getStackOriginalImage().shape
             mask = scattermask.reshape(shape)
         else:
             mask = scattermask
-        with self._scatterMaskDisconnected():
+        with self._scatterMaskDisconnected(backend):
             self.setStackSelectionMask(mask)
 
     def _getNumStackPoints(self):
         stack_images, stack_names = self.getStackROIImagesAndNames()
         return stack_images[0].size
 
+    def _getStackPositioners(self):
+        info = self.getStackInfo()
+        return info.get("positioners", {})
+
     def stackUpdated(self):
-        if not self._isScatterViewVisible():
-            return
-        self._setData()
-        self._axesSelector.setNumPoints(self._getNumStackPoints())
-        self._axesSelector.fillPositioners(self._getStackPositioners())
+        for backend in self._backends:
+            if not self._isScatterViewVisible(backend):
+                return
+            self._setData(backend)
+            self._axesSelectors[backend].setNumPoints(self._getNumStackPoints())
+            self._axesSelectors[backend].fillPositioners(self._getStackPositioners())
 
     def selectionMaskUpdated(self):
-        if not self._isScatterViewVisible():
-            return
-        mask = self.getStackSelectionMask()
-        scatterMask = mask.reshape((-1,))
-        self._scatterView.getMaskToolsWidget().setSelectionMask(scatterMask)
+        for backend in self._backends:
+            if not self._isScatterViewVisible(backend):
+                return
+            mask = self.getStackSelectionMask()
+            scatterMask = mask.reshape((-1,))
+            self._scatterViews[backend].getMaskToolsWidget().setSelectionMask(scatterMask)
 
     def stackClosed(self):
-        if self._scatterView is not None:
-            self._scatterView.close()
+        for sv in self._scatterViews.values():
+            if sv is not None:
+                sv.close()
 
     def stackROIImageListUpdated(self):
         self.stackUpdated()
