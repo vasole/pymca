e70e836fb2bd696284426559496bb3cad8a564a5
V. Armando Solé <vasole@users.noreply.github.com>
2018-12-12 15:31:57 +0100

    Update StackPluginBase.py

--- PyMca5/PyMcaCore/StackPluginBase.py
+++ PyMca5/PyMcaCore/StackPluginBase.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2017 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2018 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -38,7 +38,7 @@ in order to perform user defined operations on the data stack.
 Plugins can be automatically installed provided they are in the appropriate place:
 
     - In the user home directory (POSIX systems): *${HOME}/.pymca/plugins* 
-      or *${HOME}/PyMca5/plugins* (older PyMca installation)
+      or *${HOME}/PyMca/plugins* (older PyMca installation)
     - In *"My Documents\\\\PyMca\\\\plugins"* (Windows)
 
 It has to inherit the :class:`StackPluginBase` class and implement the following methods:
