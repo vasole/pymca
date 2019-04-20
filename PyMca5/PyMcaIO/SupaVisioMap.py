ff8717ef383b06dfda5a90df3cb1e3892c6d9754
V. Armando Solé <sole@esrf.fr>
2019-04-21 00:10:45 +0200

    No distinction between windows and other platforms.

--- PyMca5/PyMcaIO/SupaVisioMap.py
+++ PyMca5/PyMcaIO/SupaVisioMap.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2014 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2019 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -49,10 +49,7 @@ class SupaVisioMap(DataObject.DataObject):
         DataObject.DataObject.__init__(self)
 
         fileSize = os.path.getsize(filename)
-        if sys.platform == 'win32':
-            fid = open(filename, 'rb')
-        else:
-            fid = open(filename, 'r')
+        fid = open(filename, 'rb')
         data=fid.read()
         fid.close()
         self.sourceName = [filename]
