610929be2f1089aa155f73e3c53947a194991c5c
woutdenolf <woutdenolf@users.sf.net>
2019-05-30 11:00:10 +0200

    [extimg] prepare plugin for HDF5 support

--- PyMca5/PyMcaPlugins/ExternalImagesStackPlugin.py
+++ PyMca5/PyMcaPlugins/ExternalImagesStackPlugin.py
@@ -112,7 +112,6 @@ class ExternalImagesStackPlugin(StackPluginBase.StackPluginBase):
     def _loadImageFiles(self):
         if self.getStackDataObject() is None:
             return
-        getfilter = True
         fileTypeList = ["PNG Files (*png)",
                         "JPEG Files (*jpg *jpeg)",
                         "IMAGE Files (*)",
@@ -120,148 +119,154 @@ class ExternalImagesStackPlugin(StackPluginBase.StackPluginBase):
                         "CSV Files (*csv)",
                         "EDF Files (*edf)",
                         "EDF Files (*ccd)",
-                        "EDF Files (*)"]
-        message = "Open image file"
+                        "EDF Files (*)",
+                        'HDF5 Files (*.h5 *.nxs *.hdf *.hdf5)']
         filenamelist, filefilter = PyMcaFileDialogs.getFileList(parent=None,
                                     filetypelist=fileTypeList,
-                                    message=message,
-                                    getfilter=getfilter,
+                                    message="Open image file",
+                                    getfilter=True,
                                     single=False,
                                     currentfilter=None)
-        if len(filenamelist) < 1:
+        if not filenamelist:
             return
-        imagelist = []
-        imagenames= []
+        filefilter = filefilter.split()[0].lower()
+        extension = qt.safe_str(os.path.splitext(filenamelist[0])[1]).lower()
+        if (filefilter in ["edf"]) or \
+           (extension in [".edf", ".tif"]):
+            self._createWidgetEdf(filenamelist)
+        elif (filefilter in ["hdf5"]) or \
+           (extension in [".h5", ".nxs", ".hdf", ".hdf5"]):
+            self._createWidgetHdf5(filenamelist)
+        elif extension in [".csv", ".dat"]:
+            self._createWidgetSpec(filenamelist)
+        else:
+            self._createWidgetQImageReadable(filenamelist)
+
+    @property
+    def requiredShape(self):
         mask = self.getStackSelectionMask()
         if mask is None:
             r, n = self.getStackROIImagesAndNames()
-            shape = r[0].shape
+            return r[0].shape
         else:
-            shape = mask.shape
-        extension = qt.safe_str(os.path.splitext(filenamelist[0])[1])
-        if (filefilter.split()[0] in ["EDF"]) or \
-           (extension.upper() in [".EDF", ".TIF"]):
-            for filename in filenamelist:
-                #read the edf file
-                edf = EDFStack.EdfFileDataSource.EdfFileDataSource(filename)
+            return mask.shape
 
-                #the list of images
-                keylist = edf.getSourceInfo()['KeyList']
-                if len(keylist) < 1:
-                    msg = qt.QMessageBox(None)
-                    msg.setIcon(qt.QMessageBox.Critical)
-                    msg.setText("Cannot read image from file")
-                    msg.exec_()
-                    return
-
-                for key in keylist:
-                    #get the data
-                    dataObject = edf.getDataObject(key)
-                    data = dataObject.data
-                    if data.shape[0] not in shape:
-                        continue
-                    if data.shape[1] not in shape:
-                        continue
-                    imagename  = dataObject.info.get('Title', "")
-                    if imagename != "":
-                        imagename += " "
-                    imagename += os.path.basename(filename)+" "+key
-                    imagelist.append(data)
-                    imagenames.append(imagename)
-            if len(imagelist) == 0:
+    def _createWidgetEdf(self, filenamelist):
+        imagelist = []
+        imagenames = []
+        shape = self.requiredShape
+        for filename in filenamelist:
+            edf = EDFStack.EdfFileDataSource.EdfFileDataSource(filename)
+            keylist = edf.getSourceInfo()['KeyList']
+            if len(keylist) < 1:
                 msg = qt.QMessageBox(None)
                 msg.setIcon(qt.QMessageBox.Critical)
-                msg.setText("Cannot read a valid image from the file")
+                msg.setText("Cannot read image from file")
                 msg.exec_()
                 return
-            crop = False
-            self.widget = StackPluginResultsWindow.StackPluginResultsWindow(parent=None,
-                                                    usetab=False)
-            self.widget.buildAndConnectImageButtonBox()
-            self.widget.sigMaskImageWidgetSignal.connect(self.mySlot)
-            self.widget.setStackPluginResults(imagelist,
-                                              image_names=imagenames)
-            self._showWidget()
-            return
-        elif extension.lower() in [".csv", ".dat"]:
-            # what to do if more than one file selected ?
-            from PyMca5.PyMca import specfilewrapper as Specfile
-            sf = Specfile.Specfile(filenamelist[0])
-            scan = sf[0]
-            labels = scan.alllabels()
-            data = scan.data()
-            scan = None
-            sf = None
-            if "column" in labels:
-                offset = labels.index("column")
-                ncols = int(data[offset].max() + 1)
-                offset += 1
-            else:
-                raise IOError("Only images exported as csv supported")
-            imagelist = []
-            imagenames= []
-            for i in range(offset, len(labels)):
-                if labels[i].startswith("s("):
+            for key in keylist:
+                dataObject = edf.getDataObject(key)
+                data = dataObject.data
+                if data.shape[0] not in shape:
                     continue
-                tmpData = data[i]
-                tmpData.shape = -1, ncols
-                imagelist.append(tmpData)
-                imagenames.append(labels[i])
-            crop = False
-            self.widget = StackPluginResultsWindow.StackPluginResultsWindow(parent=None,
-                                                    usetab=False)
-            self.widget.buildAndConnectImageButtonBox()
-            self.widget.sigMaskImageWidgetSignal.connect(self.mySlot)
-            self.widget.setStackPluginResults(imagelist,
-                                              image_names=imagenames)
-            self._showWidget()
-            return
-        else:
-            #Try pure Image formats
-            for filename in filenamelist:
-                image = qt.QImage(filename)
-                if image.isNull():
-                    msg = qt.QMessageBox(None)
-                    msg.setIcon(qt.QMessageBox.Critical)
-                    msg.setText("Cannot read file %s as an image" % filename)
-                    msg.exec_()
-                    return
-                imagelist.append(image)
-                imagenames.append(os.path.basename(filename))
+                if data.shape[1] not in shape:
+                    continue
+                imagename = dataObject.info.get('Title', "")
+                if imagename != "":
+                    imagename += " "
+                imagename += os.path.basename(filename)+" "+key
+                imagelist.append(data)
+                imagenames.append(imagename)
+        self._createStackPluginResultsWindow(imagenames, imagelist)
 
-            if len(imagelist) == 0:
+    def _createWidgetHdf5(self, filenamelist):
+        raise NotImplementedError
+
+    def _createWidgetSpec(self, filenamelist):
+        # what to do if more than one file selected ?
+        from PyMca5.PyMca import specfilewrapper as Specfile
+        sf = Specfile.Specfile(filenamelist[0])
+        scan = sf[0]
+        labels = scan.alllabels()
+        data = scan.data()
+        scan = None
+        sf = None
+        if "column" in labels:
+            offset = labels.index("column")
+            ncols = int(data[offset].max() + 1)
+            offset += 1
+        else:
+            raise IOError("Only images exported as csv supported")
+        imagelist = []
+        imagenames = []
+        for i in range(offset, len(labels)):
+            if labels[i].startswith("s("):
+                continue
+            tmpData = data[i]
+            tmpData.shape = -1, ncols
+            imagelist.append(tmpData)
+            imagenames.append(labels[i])
+        self._createStackPluginResultsWindow(imagenames, imagelist)
+
+    def _createWidgetQImageReadable(self, filenamelist):
+        imagelist = []
+        imagenames = []
+        for filename in filenamelist:
+            image = qt.QImage(filename)
+            if image.isNull():
                 msg = qt.QMessageBox(None)
                 msg.setIcon(qt.QMessageBox.Critical)
-                msg.setText("Cannot read a valid image from the file")
+                msg.setText("Cannot read file %s as an image" % filename)
                 msg.exec_()
                 return
-            self.widget = ExternalImagesWindow.ExternalImagesWindow(parent=None,
-                                                    rgbwidget=None,
-                                                    selection=True,
-                                                    colormap=True,
-                                                    imageicons=True,
-                                                    standalonesave=True)
-            self.widget.buildAndConnectImageButtonBox()
-            self.widget.sigMaskImageWidgetSignal.connect(self.mySlot)
-            self.widget.setImageData(None)
-            self.widget.setQImageList(imagelist, shape[1], shape[0],
-                                                clearmask=False,
-                                                data=None,
-                                                imagenames=imagenames)
-                                                #data=self.__stackImageData)
-            self._showWidget()
-            return
+            imagelist.append(image)
+            imagenames.append(os.path.basename(filename))
+        self._createExternalImagesWindow(imagenames, imagelist)
 
+    def _createExternalImagesWindow(self, imagenames, imagelist):
+        if not imagenames:
+            msg = qt.QMessageBox(None)
+            msg.setIcon(qt.QMessageBox.Critical)
+            msg.setText("No valid data provided")
+            msg.exec_()
+            return
+        self.widget = ExternalImagesWindow.ExternalImagesWindow(parent=None,
+                                                rgbwidget=None,
+                                                selection=True,
+                                                colormap=True,
+                                                imageicons=True,
+                                                standalonesave=True)
+        self.widget.buildAndConnectImageButtonBox()
+        self.widget.sigMaskImageWidgetSignal.connect(self.mySlot)
+        self.widget.setImageData(None)
+        shape = self.requiredShape
+        self.widget.setQImageList(imagelist, shape[1], shape[0],
+                                  clearmask=False,
+                                  data=None,
+                                  imagenames=imagenames)
+                                  #data=self.__stackImageData)
+        self._showWidget()
+
+    def _createStackPluginResultsWindow(self, imagenames, imagelist):
+        if not imagenames:
+            msg = qt.QMessageBox(None)
+            msg.setIcon(qt.QMessageBox.Critical)
+            msg.setText("No valid data provided")
+            msg.exec_()
+            return
+        self.widget = StackPluginResultsWindow.StackPluginResultsWindow(parent=None,
+                                                usetab=False)
+        self.widget.buildAndConnectImageButtonBox()
+        self.widget.sigMaskImageWidgetSignal.connect(self.mySlot)
+        self.widget.setStackPluginResults(imagelist,
+                                          image_names=imagenames)
+        self._showWidget()
 
     def _showWidget(self):
         if self.widget is None:
             return
-
-        #Show
         self.widget.show()
         self.widget.raise_()
-
-        #update
         self.selectionMaskUpdated()
 
 
