3305317b99e7c283bedfb843e27c1d143566635b
V. Armando Solé <vasole@users.noreply.github.com>
2019-07-10 11:02:06 +0200

    Add missing import

--- PyMca5/PyMcaMath/linalg.py
+++ PyMca5/PyMcaMath/linalg.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2016 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2019 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -31,6 +31,7 @@ __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
 import numpy
+import sys
 __doc__ = """
 
 lstsq
