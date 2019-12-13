f1feab9643745e87875f94efd03ff114ebe03645
V. Armando Solé <sole@esrf.fr>
2019-12-13 10:43:11 +0100

    In case of error reading the file it is clear it is not an Specfile.

--- PyMca5/PyMcaIO/specfilewrapper.py
+++ PyMca5/PyMcaIO/specfilewrapper.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2015 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2019 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -103,7 +103,11 @@ def Specfile(filename):
                    ('#SPECTRUM' in line):
                     line = ""
                 break
-        line = f.readline()
+        try:
+            line = f.readline()
+        except:
+            line = ""
+            break
     f.close()
     amptek = False
     qxas   = False
