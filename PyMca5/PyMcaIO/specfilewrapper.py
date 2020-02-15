b4769a5dcca61ef2d2a3ae8b44d5622b0b575f20
V. Armando Solé <sole@esrf.fr>
2020-02-15 16:48:27 +0100

    Universal newlines does not seem to work properly on MacOS.

--- PyMca5/PyMcaIO/specfilewrapper.py
+++ PyMca5/PyMcaIO/specfilewrapper.py
@@ -2,7 +2,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2019 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2020 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
@@ -75,7 +75,7 @@ else:
         return var[0]
 
 def Specfile(filename):
-    f = open(filename)
+    f = open(filename, 'r', errors="ignore")
     line0  = f.readline()
     if filename.upper().endswith('DTA'):
         #TwinMic single column file
