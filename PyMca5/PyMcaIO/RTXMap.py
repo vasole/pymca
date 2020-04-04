171a8687e6423f6c559a1623bebf408de9587a60
V. Armando Solé <sole@esrf.fr>
2020-04-05 00:32:31 +0200

    Refactor and obtain map theoretical scales.

--- PyMca5/PyMcaIO/RTXMap.py
+++ PyMca5/PyMcaIO/RTXMap.py
@@ -62,55 +62,21 @@ class RTXMap(DataObject.DataObject):
         '''
         if not os.path.exists(filename):
             raise IOError("File %s does not exists"  % filename)
-        import time
-        t0 = time.time()
-        f = ElementTree.parse(filename)
-        root = f.getroot()
-        node = root.find(".//ClassInstance[@Type='TScanInfo']")
-        scanInfoKeys = ["XFirst",
-                        "YFirst",
-                        "ZFirst",
-                        "XLast",
-                        "YLast",
-                        "ZLast",
-                        "MeasNo",
-                        "Mapping"]
-        scanInfo = {}
-        if node:
-            for child in node:
-                if child.tag in scanInfoKeys:
-                    key = child.tag
-                    if key == "Mapping":
-                        if child.text.upper() == "TRUE":
-                            scanInfo[key] = True
-                        else:
-                            scanInfo[key] = False
-                    else:
-                        scanInfo[key] = myFloat(child.text)
 
-        for key in scanInfoKeys:
-            if key in scanInfo:
-                continue
-            if key == "Mapping":
-                scanInfo[key] = False
-            else:
-                scanInfo[key] = numpy.nan
-        
-        t0 = time.time()
         if 0:
             # this works but it is very slow
-            child = None
-            node = None
-            root = None
-            f = None
             DataObject.DataObject.__init__(self)
             stack = SpecFileStack.SpecFileStack(filename)
             self.data = stack.data
             self.info = stack.info
-            print("ELAPSED = ", time.time() - t0)
             return
+
         sf = ArtaxFileParser.ArtaxFileParser(filename)
-        nscans = sf.scanno()
+        # get some Artax map specific information
+        tScanInfo = sf.artaxTScanInfo
+
+        # the rest is specfile like
+        nScans = sf.scanno()
         scan = sf[0]
         calib0 = scan.header("@CALIB")
         ctime = scan.header("@CTIME")
@@ -120,13 +86,13 @@ class RTXMap(DataObject.DataObject):
         if len(motorNames):
             positioners = {}
             for mne in motorNames:
-                positioners[mne] = numpy.zeros((nscans,),
+                positioners[mne] = numpy.zeros((nScans,),
                                                dtype=numpy.float32)
-        liveTime = numpy.zeros((nscans,), dtype=numpy.float32)
+        liveTime = numpy.zeros((nScans,), dtype=numpy.float32)
         spectrum = sf[0].mca(1)
-        data = numpy.zeros((1, nscans, len(spectrum)),
+        data = numpy.zeros((1, nScans, len(spectrum)),
                             dtype=numpy.float32)
-        for i in range(nscans):
+        for i in range(nScans):
             scan = sf[i]
             ctime = scan.header("@CTIME")[0]
             liveTime[i] = myFloat(ctime.split()[-2])
@@ -143,33 +109,33 @@ class RTXMap(DataObject.DataObject):
         self.info["McaLiveTime"] = liveTime
         self.info["McaCalib"] = [myFloat(x) for x in calib0[0].split()[1:]]
         self.info["SourceName"] = os.path.abspath(filename)
-        print("ELAPSED = ", time.time() - t0)
-        if scanInfo.get("Mapping", False):
+        
+        if tScanInfo.get("Mapping", False):
             _logger.info("Regular Artax Map")
         else:
             _logger.info("Non-regular Artax Map")
-        return
+
         # we are supposed to have a regular map
         # let's figure out the shape?
-        if scanInfo["XFirst"] == scanInfo["XLast"] and \
-           scanInfo["YFirst"] != scanInfo["YLast"] and \
-           scanInfo["ZFirst"] != scanInfo["ZLast"]:
+        if tScanInfo["XFirst"] == tScanInfo["XLast"] and \
+           tScanInfo["YFirst"] != tScanInfo["YLast"] and \
+           tScanInfo["ZFirst"] != tScanInfo["ZLast"]:
             _logger.info("YZ scan if Y and Z are finite")
             meshType = "YZ"
-        elif scanInfo["XFirst"] != scanInfo["XLast"] and \
-             scanInfo["YFirst"] != scanInfo["YLast"] and \
-             (scanInfo["ZFirst"] == scanInfo["ZLast"]:
+        elif tScanInfo["XFirst"] != tScanInfo["XLast"] and \
+             tScanInfo["YFirst"] != tScanInfo["YLast"] and \
+             tScanInfo["ZFirst"] == tScanInfo["ZLast"]:
             _logger.info("XY scan if X and Y are finite")
             meshType = "XY"            
-        elif scanInfo["XFirst"] != scanInfo["XLast"] and \
-             scanInfo["YFirst"] == scanInfo["YLast"] and \
-             (scanInfo["ZFirst"] != scanInfo["ZLast"]:
+        elif tScanInfo["XFirst"] != tScanInfo["XLast"] and \
+             tScanInfo["YFirst"] == tScanInfo["YLast"] and \
+             tScanInfo["ZFirst"] != tScanInfo["ZLast"]:
             _logger.info("XZ scan if X and Z are finite")
             meshType = "XZ"
-        elif scanInfo["XFirst"] != scanInfo["XLast"] and \
-             scanInfo["YFirst"] != scanInfo["YLast"] and \
-             numpy.isnan(scanInfo["ZFirst"]) and \
-             numpy.isnan(scanInfo["ZLast"]):
+        elif tScanInfo["XFirst"] != tScanInfo["XLast"] and \
+             tScanInfo["YFirst"] != tScanInfo["YLast"] and \
+             numpy.isnan(tScanInfo["ZFirst"]) and \
+             numpy.isnan(tScanInfo["ZLast"]):
             _logger.info("XY scan if X and Y are finite")
         else:
             meshType = None
@@ -177,11 +143,61 @@ class RTXMap(DataObject.DataObject):
         if meshType:
             for axis in meshType:
                 key=axis+"First"
-                if not numpy.isfinite(scanInfo[key]):
+                if not numpy.isfinite(tScanInfo[key]):
                     meshType = None
                     break
+
+        if meshType == "XY":
+            _logger.info("XY scan")
+            x = positioners["x"]
+            y = positioners["y"]
+            xFirst = tScanInfo["XFirst"]
+            xLast = tScanInfo["XLast"]
+            yFirst = tScanInfo["YFirst"]
+            yLast = tScanInfo["YLast"]
+        elif meshType == "XZ":
+            _logger.info("XZ scan")
+            x = positioners["x"]
+            y = positioners["z"]
+            xFirst = tScanInfo["XFirst"]
+            xLast = tScanInfo["XLast"]
+            yFirst = tScanInfo["ZFirst"]
+            yLast = tScanInfo["ZLast"]
+        elif meshType == "YZ":
+            _logger.info("YZ scan")
+            x = positioners["y"]
+            y = positioners["z"]
+            xFirst = tScanInfo["YFirst"]
+            xLast = tScanInfo["YLast"]
+            yFirst = tScanInfo["ZFirst"]
+            yLast = tScanInfo["ZLast"]
+        else:
+            return
+
+        if len(x) == 1 or len(y) == 1:
+            return
+
+        reasonableDeltaX = numpy.abs(x.max() - x.min()) / (1.0 + len(x))
+        reasonableDeltaY = numpy.abs(y.max() - y.min()) / (1.0 + len(y))
+        if numpy.abs(x[1] - x[0]) > reasonableDeltaX:
+            x0 = x[0]
+            i = 1
+            while (i < len(x)) and (numpy.abs(x[i] - x[0]) > reasonableDeltaX):
+                i += 1
+            nColumns = i
+        else:
+            x0 = x[0]
+            i = 0
+            while (i < len(x)) and (numpy.abs(x[i] - x[0]) < reasonableDeltaX):
+                i += 1
+            nColumns = i
         # the scan can be in zig-zag
         # it is safer to rely on the scatter view
+        if nScans % nColumns == 0:
+            nRows = nScans // nColumns
+            self.data.shape = nRows, nColumns, -1
+            self.info["xScale"] = [xFirst, (xLast - xFirst) / nColumns]
+            self.info["yScale"] = [yFirst, (yLast - yFirst) / nRows]
 
 def isRTXMap(filename):
     try:
@@ -204,4 +220,3 @@ def test(filename):
 
 if __name__ == "__main__":
     test(sys.argv[1])
-
