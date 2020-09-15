7cbd7fc474451794919b1496338723fd4df33092
Daniel Schick <schick.daniel@gmail.com>
2020-09-15 09:55:02 +0200

    automatic step-like data detection

--- PyMca5/PyMcaMath/SpecArithmetic.py
+++ PyMca5/PyMcaMath/SpecArithmetic.py
@@ -80,8 +80,29 @@ def search_fwhm(xdata,ydata,peak=None,index=None):
         mypeak     = peak
         index_peak = index
 
-    hm = mypeak/2
+    mymin = numpy.min(ydata)
+    hm = (mypeak-mymin)/2 + mymin
     idx = index_peak
+
+    lpeak = False
+    upeak = False
+
+    if numpy.any(ydata[0:idx] < hm):
+        lpeak = True
+    if numpy.any(ydata[idx:] < hm):
+        upeak = True
+
+    if lpeak and upeak:
+        # it is a peak so we keep the data and half-max
+        pass
+    elif lpeak:
+        # it is step-like data with a positive gradient 
+        ydata = numpy.gradient(ydata)
+        hm = (numpy.max(ydata)-numpy.min(ydata))/2+numpy.min(ydata)
+    else:
+        # it is step-like data with a negative gradient
+        ydata = -1*numpy.gradient(ydata)
+        hm = (numpy.max(ydata)-numpy.min(ydata))/2+numpy.min(ydata)
 
     try:
         while ydata[idx] >= hm:
