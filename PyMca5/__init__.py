98c8380734b730f187e58b76ce05a9c633d6e34c
V. Armando Solé <vasole@users.noreply.github.com>
2020-11-09 11:09:19 +0100

    Update __init__.py

--- PyMca5/__init__.py
+++ PyMca5/__init__.py
@@ -23,11 +23,11 @@
 # THE SOFTWARE.
 #
 #############################################################################*/
-__author__ = "V.A. Sole - ESRF Data Analysis"
+__author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.6.2"
+__version__ = "5.6.3"
 
 import os
 import sys
