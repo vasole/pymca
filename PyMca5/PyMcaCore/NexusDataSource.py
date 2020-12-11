6978021ce153eb67151f65be1900d16081c3371d
V. Armando Solé <sole@esrf.fr>
2020-12-11 16:01:13 +0100

    Missing character.

--- PyMca5/PyMcaCore/NexusDataSource.py
+++ PyMca5/PyMcaCore/NexusDataSource.py
@@ -452,7 +452,7 @@ class NexusDataSource(object):
                     output.info['MotorNames'] = []
                     output.info['MotorValues'] = []
                     for key in positioners.keys():
-                        if positioners[key].dtype in [object, numpy.object]:
+                        if positioners[key].dtype in [object, numpy.object_]:
                             # not a standard value
                             _logger.info("Skipping object key %s" % key)
                             continue
