d6134e035a5aa111a5173a38123bdf71896c38b9
V. Armando Solé <sole@esrf.fr>
2021-01-25 09:16:06 +0100

    Date actualization.

--- PyMca5/PyMcaIO/BlissSpecFile.py
+++ PyMca5/PyMcaIO/BlissSpecFile.py
@@ -3,7 +3,7 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2020 European Synchrotron Radiation Facility
+# Copyright (c) 2020-2021 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
 # the ESRF by the Software group.
