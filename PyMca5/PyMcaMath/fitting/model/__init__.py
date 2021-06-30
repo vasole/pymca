adb95912e9a5220c497ed90eb729dbaf0a79e3cf
woutdenolf <woutdenolf@users.sf.net>
2021-06-30 13:09:35 +0200

    fixup

--- PyMca5/PyMcaMath/fitting/model/__init__.py
+++ PyMca5/PyMcaMath/fitting/model/__init__.py
@@ -1,9 +1,11 @@
 """Base classes for fit models and combined fit models
 """
 
-from PyMca5.PyMcaMath.fitting.model.ParameterModel import parameter_group
-from PyMca5.PyMcaMath.fitting.model.ParameterModel import linear_parameter_group
-from PyMca5.PyMcaMath.fitting.model.LeastSquaresFitModel import LeastSquaresFitModel
+from PyMca5.PyMcaMath.fitting.model.ParameterModel import (
+    parameter_group,
+    linear_parameter_group,
+)
 from PyMca5.PyMcaMath.fitting.model.LeastSquaresFitModel import (
+    LeastSquaresFitModel,
     LeastSquaresCombinedFitModel,
 )
