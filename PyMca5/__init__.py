35b22788b1fc4984444b8708483caa019d27e465
V. Armando Solé <sole@esrf.fr>
2021-11-23 13:48:14 +0100

    Update version and changelog for 5.6.6

--- PyMca5/__init__.py
+++ PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.6.5"
+__version__ = "5.6.6"
 
 import os
 import sys
