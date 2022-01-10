356e40fcd5e6d74e8c5697655346c98b389483ca
V. Armando Solé <sole@esrf.fr>
2022-01-10 14:52:45 +0100

    Do not import Object3D module.

--- PyMca5/PyMcaGui/io/SpecFileCntTable.py
+++ PyMca5/PyMcaGui/io/SpecFileCntTable.py
@@ -1,8 +1,8 @@
 #/*##########################################################################
-# Copyright (C) 2004-2017 E. Papillon, V.A. Sole, European Synchrotron Radiation Facility
+# Copyright (C) 2004-2022 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
-# the ESRF by the Software group.
+# the ESRF.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@
 # THE SOFTWARE.
 #
 #############################################################################*/
-__author__ = "E. Papillon, V.A. Sole - ESRF Software Group"
+__author__ = "E. Papillon, V.A. Sole - ESRF"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
@@ -32,7 +32,7 @@ from PyMca5.PyMcaGui import PyMcaQt as qt
 QTVERSION = qt.qVersion()
 
 try:
-    import PyMca5.Object3D
+    import PyMca5.PyMcaGui.pymca.SilxGLWindow
     OBJECT3D = True
 except:
     OBJECT3D = False
