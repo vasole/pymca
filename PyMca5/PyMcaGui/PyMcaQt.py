8ec51f9d81ffc279667e281362fda9f0f5838622
V. Armando Solé <sole@esrf.fr>
2022-01-12 14:05:13 +0100

    Try a more explicit derivation.

--- PyMca5/PyMcaGui/PyMcaQt.py
+++ PyMca5/PyMcaGui/PyMcaQt.py
@@ -145,7 +145,11 @@ elif BINDING == "PySide2":
             return super().exec_(*args, **kwargs)
 
     # QtWidgets
-    #class QApplication(_ExecMixIn, QApplication): pass
+    from PySide2.QtWidgets import QApplication as _QApplication
+    class QApplication(_QApplication):
+        def exec(self, *args, **kwargs):
+            return _QApplication.exec_()
+
     class QColorDialog(_ExecMixIn, QColorDialog): pass
     class QDialog(_ExecMixIn, QDialog): pass
     class QErrorMessage(_ExecMixIn, QErrorMessage): pass
@@ -225,7 +229,7 @@ elif BINDING == 'PySide6':
             return screen.availableGeometry().width()
 
 else:
-    raise ImportError('No Qt wrapper found. Install one of PyQt5, PyQt4, PySide or PySide2 (untested)')
+    raise ImportError('No Qt wrapper found. Install one of PyQt5, PySide2 or PySide6')
 
 # provide a exception handler but not implement it by default
 def exceptionHandler(type_, value, trace):
