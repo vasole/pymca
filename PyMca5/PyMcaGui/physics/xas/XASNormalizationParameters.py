fb4520d45eb99ebdb809adf237f42d7429dd9739
V. Armando Solé <sole@esrf.fr>
2022-01-17 13:44:52 +0100

    Use explicit imports

--- PyMca5/PyMcaGui/physics/xas/XASNormalizationParameters.py
+++ PyMca5/PyMcaGui/physics/xas/XASNormalizationParameters.py
@@ -2,10 +2,10 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2019 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2022 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
-# the ESRF by the Software group.
+# the ESRF.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@
 # THE SOFTWARE.
 #
 #############################################################################*/
-__author__ = "V. Armando Sole - ESRF Data Analysis"
+__author__ = "V. Armando Sole - ESRF"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
@@ -34,9 +34,9 @@ import os
 import sys
 import logging
 from PyMca5.PyMcaGui import PyMcaQt as qt
-from PyMca5.PyMcaGui import PyMca_Icons
-from PyMca5.PyMcaGui import XASNormalizationWindow
-from PyMca5.PyMca import XASNormalization
+from PyMca5.PyMcaGui.plotting import PyMca_Icons
+from PyMca5.PyMcaGui.physics.xas import XASNormalizationWindow
+from PyMca5.PyMcaPhysics.xas import XASNormalization
 IconDict = PyMca_Icons.IconDict
 
 _logger = logging.getLogger(__name__)
