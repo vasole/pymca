fe84c8b0557556592daca57870e6324c6b7c84a1
V. Armando Solé <sole@esrf.fr>
2022-07-08 12:21:59 +0200

    Set default to not keep aspect ration.

--- PyMca5/PyMcaGui/io/hdf5/Hdf5NodeView.py
+++ PyMca5/PyMcaGui/io/hdf5/Hdf5NodeView.py
@@ -44,52 +44,13 @@ from PyMca5.PyMcaGui.PluginsToolButton import PluginsToolButton
 
 import silx
 from silx.gui.data.DataViewerFrame import DataViewerFrame
+from silx.gui.data.DataViewer import DataViewer
 from silx.gui.data import DataViews
 from silx.gui.data import NXdataWidgets
 from silx.gui.plot import Plot1D, Plot2D
 from silx.gui import icons
 
 
-# Quick and dirty workaround for issue #173
-if silx.version == '0.7.0':
-    from silx.gui.plot.actions.control import ColormapAction
-    from silx.gui.plot.ColormapDialog import ColormapDialog
-
-    def _ColormapAction_createDialog(parent):
-        dialog = ColormapDialog()
-        dialog.setModal(False)
-        return dialog
-
-    ColormapAction._createDialog = staticmethod(_ColormapAction_createDialog)
-
-
-if silx.version == '0.7.0':
-    import sys
-    if sys.version_info < (3,):
-        import numpy
-        def _get_attr_as_string(item, attr_name, default=None):
-            attr = item.attrs.get(attr_name, default)
-            if hasattr(attr, "encode"):
-                # unicode
-                return attr.encode("utf-8")
-            elif isinstance(attr, numpy.ndarray) and not attr.shape and\
-                    hasattr(attr[()], "decode"):
-                # byte string as ndarray scalar
-                return attr[()].decode("utf-8")
-            elif isinstance(attr, numpy.ndarray) and len(attr.shape) and\
-                    hasattr(attr[0], "decode"):
-                # array of byte-strings
-                return [element.decode("utf-8") for element in attr]
-            elif isinstance(attr, numpy.ndarray) and not attr.shape:
-                # convert array to list
-                return attr[()]
-            elif isinstance(attr, numpy.ndarray) and len(attr.shape):
-                # convert array to list
-                return [element for element in attr]
-            else:
-                return attr
-        from silx.io import nxdata
-        nxdata.get_attr_as_string = _get_attr_as_string
 
 PLUGINS_DIR = []
 if os.path.exists(os.path.join(os.path.dirname(PyMca5.__file__), "PyMcaPlugins")):
@@ -157,11 +118,10 @@ class Plot2DViewWithPlugins(DataViews._Plot2dView):
         widget.setDefaultColormap(self.defaultColormap())
         widget.getColormapAction().setColorDialog(self.defaultColorDialog())
         widget.getIntensityHistogramAction().setVisible(True)
-        widget.setKeepDataAspectRatio(True)
+        widget.setKeepDataAspectRatio(False)
         widget.getXAxis().setLabel('X')
         widget.getYAxis().setLabel('Y')
         return widget
-
 
 class ArrayCurvePlotWithPlugins(NXdataWidgets.ArrayCurvePlot):
     """Adds a plugin toolbutton to an ArrayCurvePlot widget"""
@@ -245,4 +205,33 @@ class Hdf5NodeView(CloseEventNotifyingWidget.CloseEventNotifyingWidget):
         self.viewWidget.setData(dataset)
 
 
+class Hdf5NodeViewer(CloseEventNotifyingWidget.CloseEventNotifyingWidget):
+    """QWidget displaying data as raw values in a table widget, or as a
+    curve, image or stack in a plot widget. It can also display information
+    related to HDF5 groups (attributes, compression, ...) and interpret
+    a NXdata group to plot its data.
+
+    The plot features depend on *silx*'s availability.
+    """
+    def __init__(self, parent=None):
+        CloseEventNotifyingWidget.CloseEventNotifyingWidget.__init__(self,
+                                                                     parent)
+        self.mainLayout = qt.QVBoxLayout(self)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainLayout.setSpacing(0)
+
+        self.viewWidget = DataViewer(self)
+        self.viewWidget.replaceView(DataViews.PLOT1D_MODE,
+                                    Plot1DViewWithPlugins(self))
+        self.viewWidget.replaceView(DataViews.PLOT2D_MODE,
+                                    Plot2DViewWithPlugins(self))
+        self.viewWidget.replaceView(DataViews.NXDATA_CURVE_MODE,
+                                    NXdataCurveViewWithPlugins(self))
+        self.viewWidget.replaceView(DataViews.NXDATA_IMAGE_MODE,
+                                    NXdataImageViewWithPlugins(self))
+
+        self.mainLayout.addWidget(self.viewWidget)
+
+    def setData(self, dataset):
+        self.viewWidget.setData(dataset)
 
