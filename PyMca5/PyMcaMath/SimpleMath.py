facd4df8703eef3aa796453821109a1f3df3c36c
V. Armando Solé <sole@esrf.fr>
2023-01-18 13:40:59 +0100

    Deal with the case of strictly decreasing negative values

--- PyMca5/PyMcaMath/SimpleMath.py
+++ PyMca5/PyMcaMath/SimpleMath.py
@@ -2,10 +2,10 @@
 #
 # The PyMca X-Ray Fluorescence Toolkit
 #
-# Copyright (c) 2004-2020 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2022 European Synchrotron Radiation Facility
 #
 # This file is part of the PyMca X-ray Fluorescence Toolkit developed at
-# the ESRF by the Software group.
+# the ESRF.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@
 # THE SOFTWARE.
 #
 #############################################################################*/
-__author__ = "V.A. Sole - ESRF Data Analysis"
+__author__ = "V.A. Sole - ESRF"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
@@ -53,15 +53,17 @@ class SimpleMath(object):
             y=numpy.take(y,i1)
 
         # make sure data are strictly increasing
-        deltax=x[1:] - x[:-1]
+        deltax = x[1:] - x[:-1]
         i1=numpy.nonzero(abs(deltax)>0.0000001)[0]
         x=numpy.take(x, i1)
         y=numpy.take(y, i1)
-
+        i1 = numpy.argsort(x)
+        x=numpy.take(x, i1)
+        y=numpy.take(y, i1)
         if option is None or option.startswith("SG"):
-            minDelta = deltax[deltax > 0]
+            minDelta = deltax[numpy.abs(deltax) > 0]
             if minDelta.size:
-                minDelta = minDelta.min()
+                minDelta = numpy.abs(minDelta).min()
             else:
                 # all points are equal
                 minDelta = 1.0
