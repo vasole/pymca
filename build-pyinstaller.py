9e1d538c8b7c12bb0a9fe09cfbc6cdd08d5133fa
V. Armando Sole <sole@esrf.fr>
2023-11-24 17:06:58 +0100

    Allow to specify the architecture

--- build-pyinstaller.py
+++ build-pyinstaller.py
@@ -7,6 +7,17 @@ cmd = r"cd %s; pyinstaller pyinstaller.spec --noconfirm --workpath %s --distpath
                os.path.join(".", "build-" + sys.platform),
                os.path.join(".", "dist-" + sys.platform))
 
+if sys.platform.startswith("darwin"):
+    if "arm64" in sys.argv:
+        os.putenv("PYMCA_PYINSTALLER_TARGET_ARCH", "arm64")
+    elif "universal2" in sys.argv:
+        os.putenv("PYMCA_PYINSTALLER_TARGET_ARCH", "universal2")
+    elif "x86_64" in sys.argv:
+        os.putenv("PYMCA_PYINSTALLER_TARGET_ARCH", "x86_64")
+    else:
+        # let PyInstaller choose according to platform
+        pass
+
 if sys.platform.startswith("win"):
     cmd = cmd.replace(";", "&")    
 result = os.system(cmd)
