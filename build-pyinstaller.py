acebb663ac9e0bb53f015ac34392bb3ff2d16f7c
V. Armando Sole <sole@esrf.fr>
2023-11-24 10:56:57 +0100

    Architecture options not valid when using a .spec file.

--- build-pyinstaller.py
+++ build-pyinstaller.py
@@ -7,14 +7,6 @@ cmd = r"cd %s; pyinstaller pyinstaller.spec --noconfirm --workpath %s --distpath
                os.path.join(".", "build-" + sys.platform),
                os.path.join(".", "dist-" + sys.platform))
 
-if sys.platform.startswith("darwin"):
-    if "arm64" in sys.argv:
-        cmd += " --target-arch arm64"
-    elif "universal2" in sys.argv:
-        cmd += " --target-arch universal2"
-    else:
-        cmd += " --target-arch x86_64"
-
 if sys.platform.startswith("win"):
     cmd = cmd.replace(";", "&")    
 result = os.system(cmd)
