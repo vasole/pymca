8f929ab9d5d90ce9f98e9ee73c15b24670ffdf48
V. Armando Solé <sole@esrf.fr>
2023-12-13 13:40:32 +0100

    Get rid of obsolete code

--- PyMca5/PyMcaGui/misc/QIPythonWidget.py
+++ PyMca5/PyMcaGui/misc/QIPythonWidget.py
@@ -40,16 +40,6 @@ from PyMca5.PyMcaGui.PyMcaQt import QApplication, QWidget, \
                                     QPushButton, QVBoxLayout, QMessageBox, \
                                     BINDING
 
-# this seems to be obsolete stuff
-if BINDING == "PySide":
-    os.environ['QT_API'] = 'pyside'
-elif BINDING == "PySide2":
-    os.environ['QT_API'] = 'pyside2'
-elif BINDING == "PyQt4":
-    os.environ['QT_API'] = 'pyqt'
-else:
-    os.environ['QT_API'] = 'pyqt5'
-
 QTCONSOLE = True
 if sys.version_info < (3,):
     import IPython
