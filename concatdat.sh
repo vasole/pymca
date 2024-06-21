9dd72df7a7a5a65f2a41c1b1db606ea23a56e185
Alex Baekey <alexanderbaekey@gmail.com>
2024-06-21 10:23:47 -0400

    some changes to shell script

--- concatdat.sh
+++ concatdat.sh
@@ -1,9 +1,10 @@
-datadir=data/test_data
+datadir=../data/test_data
+resdir=../data
 
 for f in $datadir/*.dat;
-do (cat "${f}"; echo) >> temp_data2.dat;
+do (cat "${f}"; echo) >> $resdir/temp_data2.dat;
 done;
 #dos2unix temp_data2.dat; would need to install?
-sed 's/\r//' temp_data2.dat > temp_data.dat
-rm temp_data2.dat
+sed 's/\r//' $resdir/temp_data2.dat > $resdir/temp_data.dat # only if ^M character as EOL from windows
+rm $resdir/temp_data2.dat
 
